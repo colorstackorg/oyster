5cc3809c4b53595b1203280f6da00300e0b76ef6
Omar Elbaz <oe89@scarletmail.rutgers.edu>
2024-03-27 21:26:07 -0400

    Update Slack User Services

--- packages/core/src/modules/slack/services/slack-user.service.ts
+++ packages/core/src/modules/slack/services/slack-user.service.ts
@@ -1,5 +1,7 @@
 import { slack } from '@/modules/slack/instances';
 import { ENV } from '@/shared/env';
+import { Member } from '@slack/web-api/dist/response/UsersListResponse';
+import { Profile } from '@slack/web-api/dist/response/UsersProfileGetResponse';
 
 /**
  * @see https://api.slack.com/methods/users.lookupByEmail
@@ -34,4 +36,54 @@ export async function updateSlackEmail(id: string, email: string) {
     token: ENV.SLACK_ADMIN_TOKEN,
     user: id,
   });
+}
+/**
+ * @see https://api.slack.com/methods/users.list
+ */
+export async function getAllSlackUsers() {
+  let allUsers: Member[] = [];
+  let cursor; // Slack uses cursor-based pagination.
+
+  try {
+    // Continue calling the API until there's no more pages (cursor is empty).
+    do {
+      const response = await slack.users.list({ cursor: cursor });
+      const users: Member[] | undefined = response.members;
+
+      if (!users) {
+        break;
+      }
+
+      allUsers.push(...users);
+
+      // Slack recommends waiting for a short period of time before next pagination request
+      await new Promise((resolve) => setTimeout(resolve, 200));
+
+      // Update cursor to the next cursor value, if any.
+      cursor = response.response_metadata?.next_cursor;
+    } while (cursor);
+
+    return allUsers;
+  } catch (e) {
+    console.error('Failed to fetch all Slack users:', e);
+    return null;
+  }
+}
+
+export async function getUserProfile(user: Member) {
+  let userId = user.id;
+  try {
+    // Continue calling the API until there's no more pages (cursor is empty).
+    const response = await slack.users.profile.get({ id: userId });
+    const userProfile: Profile | undefined = response.profile;
+
+    return userProfile;
+  } catch (e) {
+    console.error(
+      'Failed to fetch all User Profile for userId= %d:',
+      userId,
+      e
+    );
+    return null;
+  }
 }
