03761ab8680db6e61c6c66abf12afe7bcce46a37
Rami Abdou <ramiabdou98@gmail.com>
2024-03-28 15:13:14 -0700

    add tailwind config for pretteir

--- apps/member-profile/tailwind.config.js
+++ apps/member-profile/tailwind.config.js
@@ -1,30 +1,7 @@
+import { tailwindConfig } from '@oyster/tailwind';
+
 /** @type {import('tailwindcss').Config} */
 export default {
+  ...tailwindConfig,
   content: ['./app/**/*.{ts,tsx}', '../../packages/ui/src/**/*.tsx'],
-  theme: {
-    extend: {
-      colors: {
-        primary: 'var(--color-primary)',
-      },
-      keyframes: {
-        'modal-animation': {
-          '0%': { opacity: 0, transform: 'scale(0)' },
-          '100%': { opacity: 1, transform: 'scale(1)' },
-        },
-        'modal-animation-mobile': {
-          '0%': { bottom: '-100vh' },
-          '100%': { bottom: '0vh' },
-        },
-        'modal-shader-animation': {
-          '0%': { opacity: 0 },
-          '100%': { opacity: 0.75 },
-        },
-        'slide-from-left': {
-          '0%': { left: '-100%' },
-          '100%': { left: '0%' },
-        },
-      },
-    },
-  },
-  plugins: [require('@tailwindcss/container-queries')],
 };
