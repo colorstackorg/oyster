025a0bb387f6eee3b87ef2b9576b6fdf8b32fe04
hebronmekuria <hebron.mekuria@emu.edu>
2024-03-29 22:47:18 -0400

    created uploadjoboffer function

---
+++ packages/core/src/modules/employment/use-cases/upload-job-offer.ts
@@ -0,0 +1,49 @@
+import { id } from '@colorstack/utils';
+
+import { db } from '@/infrastructure/database';
+import { UploadJobOfferInput } from '../employment.types';
+
+/**
+ * Adds a job offer to a student's profile.
+ */
+export async function uploadJobOffer({
+  companyId,
+  studentId,
+  baseSalary,
+  bonus,
+  compensationType,
+  employmentType,
+  hourlyPay,
+  otherCompany,
+  startDate,
+  status,
+  stockPerYear,
+  location,
+  locationCoordinates,
+  locationType,
+}: UploadJobOfferInput) {
+  const jobOfferId = id();
+
+  await db.transaction().execute(async (trx) => {
+    await trx
+      .insertInto('job_offers')
+      .values({
+        id: id(),
+        baseSalary,
+        bonus,
+        companyId,
+        compensationType,
+        employmentType,
+        hourlyPay,
+        otherCompany,
+        startDate,
+        status,
+        stockPerYear,
+        studentId,
+        location,
+        locationCoordinates,
+        locationType,
+      })
+      .execute();
+  });
+}
