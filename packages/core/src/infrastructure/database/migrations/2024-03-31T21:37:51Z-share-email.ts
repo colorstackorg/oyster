32f7abcec2da4bfa3709373ecd3719a17cab2f01
tomas-salgado <tas268@corenll.edu>
2024-04-01 17:57:05 -0400

    clean up db migration file

--- packages/core/src/infrastructure/database/migrations/2024-03-31T21:37:51Z-share-email.ts
+++ packages/core/src/infrastructure/database/migrations/2024-03-31T21:37:51Z-share-email.ts
@@ -3,22 +3,8 @@ import { Kysely } from 'kysely';
 export async function up(db: Kysely<any>) {
   await db.schema
     .alterTable('students')
-    .addColumn('allow_email_share', 'boolean')
-    .execute();
-
-  await db.updateTable('students').set({ allow_email_share: true }).execute();
-
-  await db.schema
-    .alterTable('students')
-    .alterColumn('allow_email_share', (column) => {
-      return column.setDefault(true);
-    })
-    .execute();
-
-  await db.schema
-    .alterTable('students')
-    .alterColumn('allow_email_share', (column) => {
-      return column.setNotNull();
+    .addColumn('allow_email_share', 'boolean', (column) => {
+      return column.notNull().defaultTo(true);
     })
     .execute();
 }
@@ -27,20 +13,5 @@ export async function down(db: Kysely<any>) {
   await db.schema
     .alterTable('students')
     .dropColumn('allow_email_share')
-    .execute();
-
-  await db.schema
-    .alterTable('students')
-    .alterColumn('allow_email_share', (column) => {
-      return column.dropNotNull();
-    })
-
-    .execute();
-
-  await db.schema
-    .alterTable('students')
-    .alterColumn('allow_email_share', (column) => {
-      return column.dropDefault();
-    })
     .execute();
 }
