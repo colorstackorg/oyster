60174454057f38ba0fd6f5bd58612e86aaba08cf
Rami Abdou <ramiabdou98@gmail.com>
2024-04-03 08:50:46 -0700

    refactor truncate

---
+++ packages/core/src/infrastructure/database/shared/truncate.ts
@@ -0,0 +1,31 @@
+import { Transaction, sql } from 'kysely';
+import { DB } from 'kysely-codegen/dist/db';
+
+import { IS_PRODUCTION } from '@/shared/env';
+
+/**
+ * Truncates all tables in the database - wiping all rows, but does not affect
+ * the schema itself. This can only be used in development/test environments.
+ *
+ * @see https://www.postgresql.org/docs/current/sql-truncate.html
+ */
+export async function truncate(trx: Transaction<DB>) {
+  if (IS_PRODUCTION) {
+    return;
+  }
+
+  const tables = await trx.introspection.getTables();
+
+  const names = tables
+    .filter((table) => {
+      // We don't want to wipe the kysely tables, which track migrations b/c
+      // migrations should only be run once.
+      return !table.name.includes('kysely_');
+    })
+    .map((table) => {
+      return table.name;
+    })
+    .join(', ');
+
+  await sql`truncate table ${sql.raw(names)} cascade;`.execute(trx);
+}
