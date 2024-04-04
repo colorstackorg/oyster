f45c689c44d15c22aae319c9833fc1f53267771f
Rami Abdou <ramiabdou98@gmail.com>
2024-04-04 10:12:54 -0700

    allow _ for unused var

--- config/eslint/base.js
+++ config/eslint/base.js
@@ -15,4 +15,7 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   root: true,
+  rules: {
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+  },
 };
