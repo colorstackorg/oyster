6b175a888a685ab636324e308f66bd8cf7a78e2d
Rami Abdou <ramiabdou98@gmail.com>
2024-04-04 06:52:09 -0700

    fix

--- apps/api/.eslintrc.js
+++ apps/api/.eslintrc.js
@@ -1,6 +1,6 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   extends: ['../../config/eslint/base.js'],
-  ignorePatterns: ['/*', '!/src/**/*.{ts,tsx}'],
+  ignorePatterns: ['/*', '!/src'],
   root: true,
 };
