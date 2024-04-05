5510d0800caf1d08c3f143871a4cbefa8fe8e049
Rami Abdou <ramiabdou98@gmail.com>
2024-04-05 16:01:40 -0700

    try

--- packages/db/src/scripts/windows.js
+++ packages/db/src/scripts/windows.js
@@ -2,37 +2,28 @@ import { promises as fs } from 'fs';
 import path from 'path';
 import { fileURLToPath, pathToFileURL } from 'url';
 
-class FileMigrationProvider {
-  #props;
-  constructor(props) {
-    this.#props = props;
-  }
-  async getMigrations() {
-    const migrations = {};
-    console.log('1', this.#props.migrationFolder);
-    const files = await this.#props.fs.readdir(this.#props.migrationFolder);
-    console.log('2');
-    for (const fileName of files) {
-      const name = this.#props.path.join(this.#props.migrationFolder, fileName);
-      const migration = await import(name);
-      const migrationKey = fileName.substring(0, fileName.lastIndexOf('.'));
-    }
-    return migrations;
-  }
-}
+// class FileMigrationProvider {
+//   #props;
+//   constructor(props) {
+//     this.#props = props;
+//   }
+//   async getMigrations() {
+//     const migrations = {};
+//     console.log('1', this.#props.migrationFolder);
+//     const files = await this.#props.fs.readdir(this.#props.migrationFolder);
+//     console.log('2');
+//     for (const fileName of files) {
+//       const name = this.#props.path.join(this.#props.migrationFolder, fileName);
+//       const migration = await import(name);
+//       const migrationKey = fileName.substring(0, fileName.lastIndexOf('.'));
+//     }
+//     return migrations;
+//   }
+// }
 
 (async () => {
   const __filename = fileURLToPath(import.meta.url);
   const __dirname = path.dirname(__filename);
 
-  const migrationFolder = 'file://' + path.join(__dirname, '../migrations');
-  console.log('0', migrationFolder);
-
-  const migrations = await new FileMigrationProvider({
-    fs,
-    migrationFolder,
-    path,
-  }).getMigrations();
-
-  console.log(migrations.length);
+  await fs.readdir(path.resolve(__dirname, '../migrations'));
 })();
