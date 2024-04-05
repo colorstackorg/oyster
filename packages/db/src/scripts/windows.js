902ed6e565f1587ebd1e73bfa01d2ad753e6535e
Rami Abdou <ramiabdou98@gmail.com>
2024-04-05 15:45:20 -0700

    log

--- packages/db/src/scripts/windows.js
+++ packages/db/src/scripts/windows.js
@@ -9,7 +9,9 @@ class FileMigrationProvider {
   }
   async getMigrations() {
     const migrations = {};
+    console.log('1', this.#props.migrationFolder);
     const files = await this.#props.fs.readdir(this.#props.migrationFolder);
+    console.log('2');
     for (const fileName of files) {
       const name = this.#props.path.join(this.#props.migrationFolder, fileName);
       console.log({ name });
