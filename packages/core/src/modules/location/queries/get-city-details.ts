1b78b09bb0631cac867f0cce339696934ca65215
Rami Abdou <ramiabdou98@gmail.com>
2024-04-07 03:40:35 -0700

    fix google maps error in development

--- packages/core/src/modules/location/queries/get-city-details.ts
+++ packages/core/src/modules/location/queries/get-city-details.ts
@@ -2,6 +2,7 @@ import { z } from 'zod';
 
 import {
   getGoogleMapsKey,
+  GoogleKeyMissingError,
   GOOGLE_PLACES_API_URL,
   GooglePlacesError,
 } from '../location.shared';
@@ -31,7 +32,13 @@ const GooglePlaceDetailsResponse = z.object({
 export async function getCityDetails(id: string) {
   const url = new URL(GOOGLE_PLACES_API_URL + '/details/json');
 
-  url.searchParams.set('key', getGoogleMapsKey());
+  const key = getGoogleMapsKey();
+
+  if (!key) {
+    throw new GoogleKeyMissingError();
+  }
+
+  url.searchParams.set('key', key);
   url.searchParams.set('fields', 'geometry,name');
   url.searchParams.set('place_id', id);
 
