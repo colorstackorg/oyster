55ec8f1ee8fe93dfa5fbc5cbe32d392e9d2709bd
Rami Abdou <ramiabdou98@gmail.com>
2024-04-08 00:40:14 -0700

    list event attendees

---
+++ packages/core/src/modules/event/queries/list-event-attendees.ts
@@ -0,0 +1,28 @@
+import { type SelectExpression } from 'kysely';
+
+import { type DB } from '@oyster/db';
+import { type EventAttendee } from '@oyster/types';
+
+import { db } from '@/infrastructure/database';
+
+type ListEventAttendeesOptions<Selection> = {
+  select: Selection[];
+  where: Pick<EventAttendee, 'eventId'>;
+};
+
+export async function listEventAttendees<
+  Selection extends SelectExpression<DB, 'eventAttendees' | 'students'>,
+>(options: ListEventAttendeesOptions<Selection>) {
+  const { select, where } = options;
+
+  const attendees = await db
+    .selectFrom('eventAttendees')
+    .leftJoin('students', 'students.id', 'eventAttendees.studentId')
+    .select(select)
+    .where('eventAttendees.eventId', '=', where.eventId)
+    .where('eventAttendees.studentId', 'is not', null)
+    .orderBy('eventAttendees.createdAt', 'asc')
+    .execute();
+
+  return attendees;
+}
