37f3507b4ea44ceb15faa7d5f2de160a7ca50049
Manny Munoz <em66@uw.edu>
2024-04-09 00:03:59 -0700

    Added integration page to profile section

---
+++ apps/member-profile/app/routes/_profile.profile.integrations.tsx
@@ -0,0 +1,53 @@
+import { json, type LoaderFunctionArgs } from '@remix-run/node';
+import { useLoaderData } from '@remix-run/react';
+import { GITHUB_OAUTH_URL } from 'app/shared/constants';
+import { getMember } from 'app/shared/queries';
+import { GitHub, Smile } from 'react-feather';
+
+import { Button, Text } from '@oyster/ui';
+
+import {
+  ProfileHeader,
+  ProfileSection,
+  ProfileTitle,
+} from '../shared/components/profile';
+import { ensureUserAuthenticated, user } from '../shared/session.server';
+
+export async function loader({ request }: LoaderFunctionArgs) {
+  const session = await ensureUserAuthenticated(request);
+
+  const id = user(session);
+
+  const clientId = process.env.GITHUB_OAUTH_CLIENT_ID;
+
+  const student = await getMember(id)
+    .select(['githubId'])
+    .executeTakeFirstOrThrow();
+
+  return json({ student, clientId });
+}
+
+export default function IntegrationsPage() {
+  const { student, clientId } = useLoaderData<typeof loader>();
+
+  const githubURL = `${GITHUB_OAUTH_URL}?client_id=${clientId}`;
+
+  return (
+    <ProfileSection>
+      <ProfileHeader>
+        <ProfileTitle>Integrations</ProfileTitle>
+      </ProfileHeader>
+
+      {student.githubId ? (
+        <Button disabled>
+          Github connected <Smile size={20} />
+        </Button>
+      ) : (
+        <Button>
+          <GitHub size={20} />
+          <a href={githubURL}>Add your Github account</a>
+        </Button>
+      )}
+    </ProfileSection>
+  );
+}
