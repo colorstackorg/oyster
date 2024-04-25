9e3961119859ed2f651e17697a56beccbd94860a
Rami Abdou <ramiabdou98@gmail.com>
2024-04-24 19:50:28 -0700

    add id and created_at

--- packages/db/src/migrations/20240425022431_feature_flag.ts
+++ packages/db/src/migrations/20240425022431_feature_flag.ts
@@ -1,14 +1,20 @@
-import { type Kysely } from 'kysely';
+import { type Kysely, sql } from 'kysely';
 
 export async function up(db: Kysely<any>) {
   await db.schema
     .createTable('feature_flags')
     .addColumn('code', 'text', (column) => {
-      return column.primaryKey();
+      return column.notNull().unique();
+    })
+    .addColumn('created_at', 'timestamptz', (column) => {
+      return column.notNull().defaultTo(sql`now()`);
     })
     .addColumn('description', 'text')
     .addColumn('enabled', 'boolean', (column) => {
       return column.notNull().defaultTo(false);
+    })
+    .addColumn('id', 'serial', (column) => {
+      return column.primaryKey();
     })
     .addColumn('name', 'text', (column) => {
       return column.notNull();
