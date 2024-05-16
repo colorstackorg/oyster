01b362799a0e6b9059a969686eb3572c098aaf67
tomas-salgado <tas268@cornell.edu>
2024-05-16 12:54:36 -0500

    change name

--- packages/db/src/migrations/20240516171520_ambassador_upload.ts
+++ packages/db/src/migrations/20240516171520_ambassador_upload.ts
@@ -3,13 +3,13 @@ import { type Kysely } from 'kysely';
 export async function up(db: Kysely<any>) {
   await db.schema
     .alterTable('onboarding_sessions')
-    .addColumn('ambassador', 'text')
+    .addColumn('uploaded_by_id', 'text')
     .execute();
 }
 
 export async function down(db: Kysely<any>) {
   await db.schema
     .alterTable('onboarding_sessions')
-    .dropColumn('ambassador')
+    .dropColumn('uploaded_by_id')
     .execute();
 }
