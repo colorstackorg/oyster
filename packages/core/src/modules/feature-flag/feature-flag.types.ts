d1fd40b3df2106007637142dad882bd9506c478b
Rami Abdou <38056800+ramiAbdou@users.noreply.github.com>
2024-05-17 12:31:18 -0700

    feat: create resume books in admin dashboard 📚 (#242)

--- packages/core/src/modules/feature-flag/feature-flag.types.ts
+++ packages/core/src/modules/feature-flag/feature-flag.types.ts
@@ -2,7 +2,7 @@ import { z } from 'zod';
 
 import { BooleanInput, Entity } from '@oyster/types';
 
-export type FeatureFlagName = 'family_application';
+export type FeatureFlagName = 'family_application' | 'resume_books';
 
 // Domain
 
