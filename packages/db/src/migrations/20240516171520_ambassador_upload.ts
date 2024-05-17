bd24f82e9bb0896fcc3a35dd93c7f42554a4e89e
tomas-salgado <tas268@cornell.edu>
2024-05-17 10:43:28 -0500

    add foreign key constraint

--- packages/db/src/migrations/20240516171520_ambassador_upload.ts
+++ packages/db/src/migrations/20240516171520_ambassador_upload.ts
@@ -5,9 +5,25 @@ export async function up(db: Kysely<any>) {
     .alterTable('onboarding_sessions')
     .addColumn('uploaded_by_id', 'text')
     .execute();
+
+  await db.schema
+    .alterTable('onboarding_sessions')
+    .addForeignKeyConstraint(
+      'fk_uploaded_by_id_admins_id',
+      ['uploaded_by_id'],
+      'admins',
+      ['id']
+    )
+    .onDelete('set null')
+    .execute();
 }
 
 export async function down(db: Kysely<any>) {
+  await db.schema
+    .alterTable('onboarding_sessions')
+    .dropConstraint('fk_uploaded_by_id_admins_id')
+    .execute();
+
   await db.schema
     .alterTable('onboarding_sessions')
     .dropColumn('uploaded_by_id')
