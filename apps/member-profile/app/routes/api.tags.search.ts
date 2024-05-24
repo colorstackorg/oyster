eb630e936c69be687bb9e10ddb4ebeaa5bf87a61
Rami Abdou <ramiabdou98@gmail.com>
2024-05-24 08:40:31 -0700

    show 25 tags

--- apps/member-profile/app/routes/api.tags.search.ts
+++ apps/member-profile/app/routes/api.tags.search.ts
@@ -10,7 +10,7 @@ export async function loader({ request }: LoaderFunctionArgs) {
   const search = new URL(request.url).searchParams.get('search') || '';
 
   const tags = await listTags({
-    pagination: { limit: 10, page: 1 },
+    pagination: { limit: 25, page: 1 },
     select: ['id', 'name'],
     where: { search },
   });
