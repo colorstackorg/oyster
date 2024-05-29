911f12949bc6e0275f329aa13646f2df6f3a3408
reaganrazon <reaganrazon@gmail.com>
2024-05-28 22:25:16 -0400

    feat: add admins table with 'view' functionality to admins dashboard 👥

--- apps/admin-dashboard/app/routes/_dashboard.admins.tsx
+++ apps/admin-dashboard/app/routes/_dashboard.admins.tsx
@@ -1,13 +1,75 @@
-import { json, type LoaderFunctionArgs } from '@remix-run/node';
+import {
+  json,
+  type LoaderFunctionArgs,
+  type SerializeFrom,
+} from '@remix-run/node';
+import { Link, Outlet, useLoaderData } from '@remix-run/react';
 
+import { Dashboard, Pill, Table, type TableColumnProps } from '@oyster/ui';
+
+import { listAdmins } from '@/admin-dashboard.server';
 import { ensureUserAuthenticated } from '@/shared/session.server';
 
 export async function loader({ request }: LoaderFunctionArgs) {
-  await ensureUserAuthenticated(request);
+  await ensureUserAuthenticated(request, {
+    allowAmbassador: true,
+  });
 
-  return json({});
+  const admins = await listAdmins();
+
+  return json({
+    admins,
+  });
 }
 
 export default function AdminsPage() {
-  return null;
+  return (
+    <>
+      <div className="flex items-center justify-between gap-4">
+        <Dashboard.Title>Admins</Dashboard.Title>
+      </div>
+
+      <AdminsTable />
+      <Outlet />
+    </>
+  );
+}
+
+type AdminInView = SerializeFrom<typeof loader>['admins'][number];
+
+function AdminsTable() {
+  const { admins } = useLoaderData<typeof loader>();
+
+  const columns: TableColumnProps<AdminInView>[] = [
+    {
+      displayName: 'First Name',
+      size: '200',
+      render: (admin) => admin.firstName,
+    },
+    {
+      displayName: 'Last Name',
+      size: '200',
+      render: (admin) => admin.lastName,
+    },
+    {
+      displayName: 'Email',
+      size: null,
+      render: (admin) => admin.email,
+    },
+    {
+      displayName: 'Status',
+      size: '200',
+      render: (admin) => {
+        return admin.isAmbassador ? (
+          <Pill color="lime-100">Ambassador</Pill>
+        ) : (
+          <Pill color="blue-100">Full</Pill>
+        );
+      },
+    },
+  ];
+
+  return (
+    <Table columns={columns} data={admins} emptyMessage="No admins found." />
+  );
 }
