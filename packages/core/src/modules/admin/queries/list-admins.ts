2de583c2372080b545f0f666d7c47a183fdaf715
Rami Abdou <38056800+ramiAbdou@users.noreply.github.com>
2024-06-04 20:51:35 -0700

    fix: show archived status for removed admins ❗️ (#291)

--- packages/core/src/modules/admin/queries/list-admins.ts
+++ packages/core/src/modules/admin/queries/list-admins.ts
@@ -3,7 +3,22 @@ import { db } from '@oyster/db';
 export async function listAdmins() {
   const admins = await db
     .selectFrom('admins')
-    .select(['firstName', 'lastName', 'email', 'isAmbassador', 'id'])
+    .select([
+      'firstName',
+      'lastName',
+      'email',
+      'isAmbassador',
+      'id',
+      (eb) => {
+        return eb
+          .case()
+          .when('deletedAt', 'is not', null)
+          .then(true)
+          .else(false)
+          .end()
+          .as('isArchived');
+      },
+    ])
     .orderBy('createdAt', 'desc')
     .execute();
 
