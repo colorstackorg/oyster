46c1e1ffe82821afaf8e28f591ae9d86ef74163c
Rami Abdou <ramiabdou98@gmail.com>
2024-06-07 09:04:17 -0700

    move companies route

--- packages/core/src/modules/employment/ui/company-field.tsx
+++ packages/core/src/modules/employment/ui/company-field.tsx
@@ -82,7 +82,7 @@ export function CompanyCombobox({
     fetcher.submit(
       { search: delayedSearch },
       {
-        action: '/companies',
+        action: '/api/companies',
         method: 'get',
       }
     );
