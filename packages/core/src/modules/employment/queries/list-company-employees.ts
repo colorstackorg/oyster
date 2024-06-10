8b5a448a53b9b83fa65fe87e06c2db70528aa079
Rami Abdou <ramiabdou98@gmail.com>
2024-06-09 20:00:07 -0700

    add context in employees list

--- packages/core/src/modules/employment/queries/list-company-employees.ts
+++ packages/core/src/modules/employment/queries/list-company-employees.ts
@@ -1,28 +1,47 @@
-import { type SelectExpression } from 'kysely';
+import { db } from '@oyster/db';
 
-import { db, type DB } from '@oyster/db';
-
-type ListCompanyEmployeesOptions<Selection> = {
-  select: Selection[];
-  where: { companyId: string };
+type ListCompanyEmployeesOptions = {
+  where: {
+    companyId: string;
+    when?: 'current' | 'past';
+  };
 };
 
-export async function listCompanyEmployees<
-  Selection extends SelectExpression<
-    DB & { employees: DB['students'] },
-    'employees'
-  >,
->({ select, where }: ListCompanyEmployeesOptions<Selection>) {
+export async function listCompanyEmployees({
+  where,
+}: ListCompanyEmployeesOptions) {
   const employees = await db
     .selectFrom('workExperiences')
-    .leftJoin(
-      'students as employees',
+    .leftJoin('students as employees', 'employees.id', 'studentId')
+    .select([
+      'employees.firstName',
       'employees.id',
-      'workExperiences.studentId'
-    )
-    .select(select)
-    .distinctOn('workExperiences.studentId')
-    .where('workExperiences.companyId', '=', where.companyId)
+      'employees.lastName',
+      'employees.profilePicture',
+      'workExperiences.locationCity',
+      'workExperiences.locationType',
+      'workExperiences.locationState',
+      'workExperiences.title',
+      (eb) => {
+        return eb
+          .case()
+          .when('endDate', '<', new Date())
+          .then('past' as const)
+          .else('current' as const)
+          .end()
+          .as('status');
+      },
+    ])
+    .distinctOn('studentId')
+    .where('companyId', '=', where.companyId)
+    .$if(where.when === 'current', (qb) => {
+      return qb.where('endDate', 'is', null);
+    })
+    .$if(where.when === 'past', (qb) => {
+      return qb.where('endDate', '<', new Date());
+    })
+    .orderBy('studentId')
+    .orderBy('endDate', 'desc')
     .execute();
 
   return employees;
