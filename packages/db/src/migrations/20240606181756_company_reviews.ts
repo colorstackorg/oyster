32679eae66736216fe12cff42d5e83a9b3284776
Rami Abdou <ramiabdou98@gmail.com>
2024-06-10 11:04:49 -0700

    consolidate reviews

--- packages/db/src/migrations/20240606181756_company_reviews.ts
+++ packages/db/src/migrations/20240606181756_company_reviews.ts
@@ -25,8 +25,32 @@ export async function up(db: Kysely<any>) {
       return column.notNull().references('work_experiences.id');
     })
     .execute();
+
+  await db.schema
+    .alterTable('completed_activities')
+    .addColumn('work_experience_id', 'text', (column) => {
+      return column.references('work_experiences.id');
+    })
+    .execute();
+
+  await db.schema
+    .createIndex('completed_activities_review_company_idx')
+    .unique()
+    .on('completed_activities')
+    .columns(['type', 'work_experience_id'])
+    .where('type', '=', 'review_company')
+    .execute();
 }
 
 export async function down(db: Kysely<any>) {
+  await db.schema
+    .dropIndex('completed_activities_review_company_idx')
+    .execute();
+
+  await db.schema
+    .alterTable('completed_activities')
+    .dropColumn('work_experience_id')
+    .execute();
+
   await db.schema.dropTable('company_reviews').execute();
 }
