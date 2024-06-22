12464abd3870d3a910a1b95f2430dac9549e44b8
LIANARO <LIANARO@UW.EDU>
2024-06-22 13:05:33 -0700

    add levels_fyi to companies database

---
+++ packages/db/src/migrations/20240622005304_add_levels_fyi.ts
@@ -0,0 +1,12 @@
+import { type Kysely } from 'kysely';
+
+export async function up(db: Kysely<any>) {
+  await db.schema
+    .alterTable('companies')
+    .addColumn('levels_fyi', 'text')
+    .execute();
+}
+
+export async function down(db: Kysely<any>) {
+  await db.schema.alterTable('companies').dropColumn('levels_fyi').execute();
+}
