21ecc49f782e6f1bba86983684b790d9c86a5130
Wilfredo Flores <wflore19@montgomerycollege.edu>
2024-07-01 15:32:10 -0400

    add: hover and drag-over styling to file drop area

--- packages/ui/src/components/file-uploader.tsx
+++ packages/ui/src/components/file-uploader.tsx
@@ -11,6 +11,7 @@ const FILE_TYPES = ['image/png', 'image/jpeg', 'application/pdf'];
 export function FileUploader({ name }: FieldProps<ResourceType>) {
   const [selectedFile, setSelectedFile] = useState<File | null>(null);
   const [fileError, setFileError] = useState<string | null>(null);
+  const [isHover, setisHover] = useState(false);
 
   const handleFileChange = (e: ChangeEvent<HTMLInputElement>): void => {
     const file = e.target.files?.[0];
@@ -50,11 +51,21 @@ export function FileUploader({ name }: FieldProps<ResourceType>) {
     <div>
       <label
         htmlFor={name}
-        className="relative flex flex-col items-center rounded-3xl border-2 border-dashed  border-gray-300 p-2 hover:cursor-pointer"
+        onDragOver={(e) => {
+          e.preventDefault();
+          setisHover(true);
+        }}
+        onDragLeave={() => setisHover(false)}
+        onDrop={() => setisHover(false)}
+        className={`relative flex flex-col items-center rounded-3xl border-2 border-dashed transition-all duration-300 ease-in-out ${
+          isHover
+            ? 'cursor-grab border-primary'
+            : 'border-gray-300 hover:cursor-grab hover:border-primary'
+        }`}
       >
         <div className="flex flex-col items-center p-4">
-          <div className="mb-2 flex w-12 items-center justify-center rounded-full bg-gray-200 p-3">
-            <Image className="text-primary" />
+          <div className="bg- mb-2 flex h-16 w-16 items-center justify-center rounded-full bg-slate-50 p-3">
+            <Image className="h-8 w-8 text-primary" />
           </div>
 
           <p className="text-center font-normal">
@@ -70,7 +81,7 @@ export function FileUploader({ name }: FieldProps<ResourceType>) {
           name={name}
           type="file"
           accept="image/png, image/jpeg, .pdf"
-          className="absolute top-0 z-10 h-full w-full rounded-3xl opacity-0 hover:cursor-grab active:cursor-grab"
+          className="absolute top-0 z-10 h-full w-full rounded-3xl opacity-0 hover:cursor-grab"
           required
           onChange={handleFileChange}
         />
@@ -87,7 +98,6 @@ export function FileUploader({ name }: FieldProps<ResourceType>) {
             </p>
             <p className="text-xs text-gray-500">
               {formatFileSize(selectedFile.size)}{' '}
-              {/* create a span that acts as a tag */}
               <span className="font-medium">
                 {selectedFile.type.split('/')[1].toUpperCase()}
               </span>
