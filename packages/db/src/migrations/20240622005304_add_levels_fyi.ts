eb4c61d6f3372c1967c71bd8161b8098a8ecf376
Rami Abdou <ramiabdou98@gmail.com>
2024-07-10 11:32:56 -0700

    add slug

--- packages/db/src/migrations/20240622005304_add_levels_fyi.ts
+++ packages/db/src/migrations/20240622005304_add_levels_fyi.ts
@@ -3,13 +3,13 @@ import { type Kysely } from 'kysely';
 export async function up(db: Kysely<any>) {
   await db.schema
     .alterTable('companies')
-    .addColumn('levels_fyi', 'text')
+    .addColumn('levels_fyi_slug', 'text')
     .execute();
 }
 
 export async function down(db: Kysely<any>) {
   await db.schema
     .alterTable('companies')
-    .dropColumn('levels_fyi')
+    .dropColumn('levels_fyi_slug')
     .execute();
 }
