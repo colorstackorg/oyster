6170a6471105cef8fde099511fe4b52bb6dd1d47
Rami Abdou <ramiabdou98@gmail.com>
2024-07-19 21:30:29 -0700

    add documentation

--- packages/core/src/modules/google-drive/index.ts
+++ packages/core/src/modules/google-drive/index.ts
@@ -12,12 +12,39 @@ const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET as string;
 // Core
 
 type UploadFileInput = {
+  /**
+   * The file to upload to Google Drive, which is sent in a `multipart/form-data`
+   * request.
+   */
   file: File;
+
+  /**
+   * If provided, this is the ID of the file to update. This will send a `PATCH`
+   * request instead of a `POST` request to the Google Drive API.
+   */
   fileId?: string;
-  fileName: string;
+
+  /**
+   * The name of the file to upload. If not provided, the file's original name
+   * will be used.
+   */
+  fileName?: string;
+
+  /**
+   * The ID of the folder to upload the file to. If not provided, the file will
+   * be uploaded to the root of the user's Google Drive.
+   */
   folderId: string;
 };
 
+/**
+ * Uploads a file to Google Drive. If a `fileId` is provided, this will update
+ * the file instead of creating a new one.
+ *
+ * Returns the ID of the file that was uploaded.
+ *
+ * @see https://developers.google.com/drive/api/v3/manage-uploads
+ */
 export async function uploadFileToGoogleDrive({
   file,
   fileId,
@@ -30,8 +57,8 @@ export async function uploadFileToGoogleDrive({
 
   const metadata = {
     mimeType: file.type,
-    name: fileName,
-    parents: !isUpdate && !!folderId ? [folderId] : undefined,
+    name: fileName || file.name,
+    parents: !!folderId && !isUpdate ? [folderId] : undefined,
   };
 
   const form = new FormData();
@@ -70,40 +97,14 @@ export async function uploadFileToGoogleDrive({
 
 // Authentication
 
-async function retrieveAccessToken() {
-  const [accessToken = '', expiresAt = '', refreshToken = ''] =
-    await Promise.all([
-      redis.get('google_drive:access_token'),
-      redis.get('google_drive:expires_at'),
-      redis.get('google_drive:refresh_token'),
-    ]);
-
-  if (!refreshToken) {
-    throw new ColorStackError().withMessage(
-      'Failed to find the Google Drive refresh token in Redis.'
-    );
-  }
-
-  // We track the expiration time of the access token, so if it has yet to
-  // expire, we can use it!
-  if (!!expiresAt && Date.now() < parseInt(expiresAt)) {
-    return accessToken;
-  }
-
-  // Otherwise, we need to refresh the access token.
-  const newCredentials = await refreshCredentials(refreshToken);
-
-  const newExpiresAt = Date.now() + newCredentials.expiresIn * 1000;
-
-  await Promise.all([
-    redis.set('google_drive:access_token', newCredentials.accessToken),
-    redis.set('google_drive:expires_at', newExpiresAt),
-  ]);
-
-  return newCredentials.accessToken;
-}
+const ACCESS_TOKEN_KEY = 'google_drive:access_token';
+const EXPIRES_AT_KEY = 'google_drive:expires_at';
+const REFRESH_TOKEN_KEY = 'google_drive:refresh_token';
 
 /**
+ * Exchanges the authorization code for Google Drive credentials. Saves the
+ * credentials in Redis.
+ *
  * @see https://developers.google.com/identity/protocols/oauth2/web-server#exchange-authorization-code
  */
 export async function saveGoogleDriveCredentials(code: string) {
@@ -112,7 +113,7 @@ export async function saveGoogleDriveCredentials(code: string) {
     client_secret: GOOGLE_CLIENT_SECRET,
     code,
     grant_type: 'authorization_code',
-    redirect_uri: `${API_URL}/oauth/google/drive`,
+    redirect_uri: API_URL + '/oauth/google/drive',
   });
 
   const response = await fetch('https://oauth2.googleapis.com/token', {
@@ -143,13 +144,44 @@ export async function saveGoogleDriveCredentials(code: string) {
   const expiresAt = Date.now() + data.expires_in * 1000;
 
   await Promise.all([
-    redis.set('google_drive:access_token', data.access_token),
-    redis.set('google_drive:expires_at', expiresAt),
-    redis.set('google_drive:refresh_token', data.refresh_token),
+    redis.set(ACCESS_TOKEN_KEY, data.access_token),
+    redis.set(EXPIRES_AT_KEY, expiresAt),
+    redis.set(REFRESH_TOKEN_KEY, data.refresh_token),
   ]);
 }
 
+async function retrieveAccessToken() {
+  const [accessToken = '', expiresAt = '', refreshToken = ''] =
+    await Promise.all([
+      redis.get(ACCESS_TOKEN_KEY),
+      redis.get(EXPIRES_AT_KEY),
+      redis.get(REFRESH_TOKEN_KEY),
+    ]);
+
+  if (!refreshToken) {
+    throw new ColorStackError().withMessage(
+      'Failed to find the Google Drive refresh token in Redis.'
+    );
+  }
+
+  // We track the expiration time of the access token, so if it has yet to
+  // expire, we can use it!
+  if (!!expiresAt && Date.now() < parseInt(expiresAt)) {
+    return accessToken;
+  }
+
+  // Otherwise, we need to refresh the access token.
+  const newAccessToken = await refreshCredentials(refreshToken);
+
+  return newAccessToken;
+}
+
 /**
+ * Refreshes the Google Drive credentials by using the refresh token. This
+ * does NOT update the refresh token, which should never change unless the user
+ * revokes access. Saves the updated credentials in Redis.
+ *
+ * Returns the new access token.
  *
  * @see https://developers.google.com/identity/protocols/oauth2/web-server#offline
  */
@@ -162,8 +194,8 @@ async function refreshCredentials(refreshToken: string) {
   });
 
   const response = await fetch('https://oauth2.googleapis.com/token', {
-    method: 'post',
     body,
+    method: 'post',
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded',
     },
@@ -174,7 +206,8 @@ async function refreshCredentials(refreshToken: string) {
   if (!response.ok) {
     throw new ColorStackError()
       .withMessage('Failed to refresh Google Drive credentials.')
-      .withContext(json);
+      .withContext(json)
+      .report();
   }
 
   const data = validate(
@@ -185,8 +218,12 @@ async function refreshCredentials(refreshToken: string) {
     json
   );
 
-  return {
-    accessToken: data.access_token,
-    expiresIn: data.expires_in,
-  };
+  const expiresAt = Date.now() + data.expires_in * 1000;
+
+  await Promise.all([
+    redis.set(ACCESS_TOKEN_KEY, data.access_token),
+    redis.set(EXPIRES_AT_KEY, expiresAt),
+  ]);
+
+  return data.access_token;
 }
