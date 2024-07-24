a21d3660a1830a7f213aae0f571e4e265940dc8b
Wilfredo Flores <wflore19@montgomerycollege.edu>
2024-07-24 00:31:46 -0400

    add: remove route

---
+++ apps/admin-dashboard/app/routes/_dashboard.admins.$id.remove.tsx
@@ -0,0 +1,89 @@
+import {
+  type ActionFunctionArgs,
+  json,
+  type LoaderFunctionArgs,
+  redirect,
+} from '@remix-run/node';
+import { Form as RemixForm, useLoaderData } from '@remix-run/react';
+
+import { db } from '@oyster/db';
+import { Button, Modal } from '@oyster/ui';
+
+import { removeAdmin } from '@/admin-dashboard.server';
+import { Route } from '@/shared/constants';
+import {
+  commitSession,
+  ensureUserAuthenticated,
+  toast,
+} from '@/shared/session.server';
+
+export async function loader({ params, request }: LoaderFunctionArgs) {
+  await ensureUserAuthenticated(request);
+
+  const admin = await db
+    .selectFrom('admins')
+    .select(['firstName', 'lastName'])
+    .where('id', '=', params.id as string)
+    .executeTakeFirst();
+
+  if (!admin) {
+    return redirect(Route['/admins']);
+  }
+
+  return json({
+    admin,
+  });
+}
+
+export async function action({ params, request }: ActionFunctionArgs) {
+  const session = await ensureUserAuthenticated(request);
+
+  const admin = await removeAdmin({ id: params.id as string });
+
+  if (!admin) {
+    throw new Response(null, { status: 404 });
+  }
+
+  toast(session, {
+    message: 'Removed member.',
+  });
+
+  return redirect(Route['/admins'], {
+    headers: {
+      'Set-Cookie': await commitSession(session),
+    },
+  });
+}
+
+export default function RemoveMemberPage() {
+  const { admin } = useLoaderData<typeof loader>();
+
+  return (
+    <Modal onCloseTo={Route['/admins']}>
+      <Modal.Header>
+        <Modal.Title>
+          Remove {admin.firstName} {admin.lastName}
+        </Modal.Title>
+        <Modal.CloseButton />
+      </Modal.Header>
+
+      <Modal.Description>
+        This is not an undoable action. All of their engagement records will be
+        deleted and they will be removed from Slack, Mailchimp and Airtable. Are
+        you sure want to remove this member?
+      </Modal.Description>
+
+      <RemixForm className="form" method="post">
+        <Button.Group>
+          <Button color="error" type="submit">
+            Remove
+          </Button>
+        </Button.Group>
+      </RemixForm>
+    </Modal>
+  );
+}
+
+export function ErrorBoundary() {
+  return <></>;
+}
--- apps/admin-dashboard/app/shared/constants.ts
+++ apps/admin-dashboard/app/shared/constants.ts
@@ -1,6 +1,7 @@
 const ROUTES = [
   '/',
   '/admins',
+  '/admins/:id/remove',
   '/applications',
   '/applications/:id',
   '/applications/:id/accept',
