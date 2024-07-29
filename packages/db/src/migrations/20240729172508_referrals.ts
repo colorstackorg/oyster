ec1535a17dc4f585a9f8f0df99ff39e59f301a27
Rami Abdou <ramiabdou98@gmail.com>
2024-07-29 13:56:46 -0700

    initial migration

---
+++ packages/db/src/migrations/20240729172508_referrals.ts
@@ -0,0 +1,44 @@
+import { type Kysely, sql } from 'kysely';
+
+export async function up(db: Kysely<any>) {
+  await db.schema
+    .createTable('referrals')
+    .addColumn('email', 'text', (column) => {
+      return column.notNull();
+    })
+    .addColumn('first_name', 'text', (column) => {
+      return column.notNull();
+    })
+    .addColumn('id', 'text', (column) => {
+      return column.primaryKey();
+    })
+    .addColumn('last_name', 'text', (column) => {
+      return column.notNull();
+    })
+    .addColumn('referred_at', 'timestamptz', (column) => {
+      return column.notNull().defaultTo(sql`now()`);
+    })
+    .addColumn('referred_id', 'text', (column) => {
+      return column.references('students.id');
+    })
+    .addColumn('status', 'text', (column) => {
+      return column.notNull();
+    })
+    .execute();
+
+  await db.schema
+    .alterTable('applications')
+    .addColumn('referral_id', 'text', (column) => {
+      return column.references('referrals.id');
+    })
+    .execute();
+}
+
+export async function down(db: Kysely<any>) {
+  await db.schema
+    .alterTable('applications')
+    .dropColumn('referral_id')
+    .execute();
+
+  await db.schema.dropTable('referrals').execute();
+}
