57f7ba44be9f3d6a77e195be3b2ac75f36669884
Rami Abdou <ramiabdou98@gmail.com>
2024-07-31 16:25:54 -0700

    update import

--- apps/admin-dashboard/app/shared/hooks.ts
+++ apps/admin-dashboard/app/shared/hooks.ts
@@ -1,9 +1,8 @@
 import { useFetcher, useMatches } from '@remix-run/react';
 import { z } from 'zod';
 
+import { AdminRole } from '@oyster/core/admins.types';
 import { type ToastProps } from '@oyster/ui';
-
-import { AdminRole } from '@/modules/admin/admin.types';
 
 /**
  * Returns the role of the logged-in admin.
