b564fa3494ba789ce78753f71e769a31438b8812
Justin H <84724234+JustinhSE@users.noreply.github.com>
2024-07-31 23:45:18 -0400

    feat: combine `#birthdays` channel messages 🎂 (#419)

--- packages/core/src/modules/member/use-cases/send-birthday-notification.ts
+++ packages/core/src/modules/member/use-cases/send-birthday-notification.ts
@@ -11,7 +11,7 @@ export async function sendBirthdayNotification(
 ) {
   const members = await db
     .selectFrom('students')
-    .select(['firstName', 'lastName', 'slackId'])
+    .select(['slackId'])
     .whereRef(
       sql`EXTRACT(MONTH FROM birthdate)`,
       '=',
@@ -25,13 +25,26 @@ export async function sendBirthdayNotification(
     .where('birthdateNotification', 'is', true)
     .execute();
 
-  members.forEach((member) => {
-    if (member.slackId) {
-      job('notification.slack.send', {
-        channel: ENV.SLACK_BIRTHDAYS_CHANNEL_ID,
-        message: `Everyone wish a happy birthday to <@${member.slackId}>! 🎉🎂🎈`,
-        workspace: 'regular',
-      });
-    }
+  // We won't send a notification if there are no members with birthdays today!
+  if (!members.length) {
+    return;
+  }
+
+  const ids = members.map((member) => {
+    return `<@${member.slackId}>`;
+  });
+
+  const last = ids.pop();
+
+  // Example (1): <@U123>
+  // Example (2): <@U123> and <@U456>
+  // Example (3): <@U123>, <@U456> and <@U789>
+  // Example (4): <@U123>, <@U456>, <@U789> and <@123>
+  const result = ids.length ? `${ids.join(', ')} and ${last}` : last;
+
+  job('notification.slack.send', {
+    channel: ENV.SLACK_BIRTHDAYS_CHANNEL_ID,
+    message: `Everyone wish a happy birthday to ${result}! 🎉🎂🎈`,
+    workspace: 'regular',
   });
 }
