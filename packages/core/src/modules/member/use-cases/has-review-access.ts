0d3a127de20f0d01a63cf7a94df98473f7873438
cadeci <ciaracade03@gmail.com>
2024-08-03 15:56:58 -0700

    fix: Remove try-catch block

--- packages/core/src/modules/member/use-cases/has-review-access.ts
+++ packages/core/src/modules/member/use-cases/has-review-access.ts
@@ -1,30 +1,21 @@
 import { db } from '@oyster/db';
 
-export async function hasReviewAccess(id: string): Promise<boolean> {
-  try {
-    // Check if the student has any work experiences
-    const workExperienceOfStudent = await db
-      .selectFrom('workExperiences')
-      .select(['studentId'])
-      .where('studentId', '=', id)
-      .executeTakeFirst();
+export async function hasReviewAccess(studentId: string): Promise<boolean> {
+  const workExperienceOfStudent = await db
+    .selectFrom('workExperiences')
+    .select(['studentId'])
+    .where('studentId', '=', studentId)
+    .executeTakeFirst();
 
-    // If the student has no work experience, return true
-    if (!workExperienceOfStudent) {
-      return true;
-    }
-
-    // Check if the student has written any reviews
-    const companyReviewsFromStudent = await db
-      .selectFrom('companyReviews')
-      .select(['studentId'])
-      .where('studentId', '=', id)
-      .executeTakeFirst();
-
-    // Return true if a review is found, otherwise false
-    return !!companyReviewsFromStudent;
-  } catch (error) {
-    console.error('Error checking review access:', error);
-    throw error;
+  if (!workExperienceOfStudent) {
+    return true;
   }
+
+  const companyReviewsFromStudent = await db
+    .selectFrom('companyReviews')
+    .select(['studentId'])
+    .where('studentId', '=', studentId)
+    .executeTakeFirst();
+
+  return !!companyReviewsFromStudent;
 }
