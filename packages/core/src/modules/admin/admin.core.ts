029b9cef55b3a9fa1d3134a7191bb8d39bcbe91b
Wilfredo Flores <wflore19@montgomerycollege.edu>
2024-08-05 14:55:56 -0400

    add: removeAdmin function

--- packages/core/src/modules/admin/admin.core.ts
+++ packages/core/src/modules/admin/admin.core.ts
@@ -156,6 +156,25 @@ export async function addAdmin({
   return success({ id: adminId });
 }
 
+/**
+ * 'Removes' ColorStack admin. This will revoke the user access to the Admin
+ *  Dashboard. Note that it shouldn't delete the record entirely, but it should
+ *  set the deletedAt timestamp, effectively archiving the admin).
+ */
+export async function removeAdmin({ id }: { id: string }) {
+  const admin = await db
+    .updateTable('admins')
+    .set({
+      deletedAt: new Date(), // not sure of the format
+    })
+    .where('id', '=', id as string)
+    .executeTakeFirst();
+
+  if (admin) {
+    return new Error('The admin does not exist.');
+  }
+}
+
 // Helpers
 
 type DoesAdminHavePermissionInput = {
