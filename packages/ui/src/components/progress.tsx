5b6723f2ddaf95a125108aec4c0e8581cf830f24
Rami Abdou <ramiabdou98@gmail.com>
2024-08-13 15:51:04 -0700

    added docs for useProgress

--- packages/ui/src/components/progress.tsx
+++ packages/ui/src/components/progress.tsx
@@ -1,7 +1,36 @@
 import { Indicator, type ProgressProps, Root } from '@radix-ui/react-progress';
 import { useEffect, useState } from 'react';
 
+export function Progress({ max = 100, value, ...props }: ProgressProps) {
+  const translateX = 100 - (value || 0);
+
+  return (
+    <Root
+      className="relative h-2 w-full overflow-hidden rounded-full bg-gray-100"
+      max={max}
+      value={value}
+      {...props}
+    >
+      <Indicator
+        className="h-full w-full bg-primary transition-transform delay-700"
+        style={{
+          // We'll effectively "push" the indicator to the left by the inverse,
+          // but it doesn't show because of the "overflow-hidden" on the parent.
+          transform: `translateX(-${translateX}%)`,
+        }}
+      />
+    </Root>
+  );
+}
+
 type ProgressOptions = {
+  /**
+   * The cooling factor to apply to the progress. This is a number between 0 and
+   * 1 that determines how much the progress should slow down as it approaches
+   * 100%.
+   *
+   * @default 0.03
+   */
   coolingFactor?: number;
 };
 
@@ -9,6 +38,11 @@ const defaultOptions: Required<ProgressOptions> = {
   coolingFactor: 0.03,
 };
 
+/**
+ * Returns a progress value that increments over time. The value will slow down
+ * according to the cooling factor as it approaches 100%. The progress will be
+ * updated every second.
+ */
 export function useProgress(options: ProgressOptions = {}) {
   const { coolingFactor } = {
     ...defaultOptions,
@@ -35,26 +69,4 @@ export function useProgress(options: ProgressOptions = {}) {
   }, []);
 
   return progress;
-}
-
-export function Progress({ max = 100, value, ...props }: ProgressProps) {
-  const translateX = 100 - (value || 0);
-
-  return (
-    <Root
-      className="relative h-2 w-full overflow-hidden rounded-full bg-gray-100"
-      max={max}
-      value={value}
-      {...props}
-    >
-      <Indicator
-        className="h-full w-full bg-primary transition-transform delay-700"
-        style={{
-          // We'll effectively "push" the indicator to the left by the inverse,
-          // but it doesn't show because of the "overflow-hidden" on the parent.
-          transform: `translateX(-${translateX}%)`,
-        }}
-      />
-    </Root>
-  );
 }
