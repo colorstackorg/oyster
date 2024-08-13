604f5e7b21069db63f875c261709312922d0ad78
Rami Abdou <ramiabdou98@gmail.com>
2024-08-13 15:24:49 -0700

    add docs to redis

--- packages/core/src/infrastructure/redis.ts
+++ packages/core/src/infrastructure/redis.ts
@@ -28,13 +28,19 @@ export type RedisKey = ExtractValue<typeof RedisKey>;
 // Constants
 
 export const ONE_MINUTE_IN_SECONDS = 60;
-export const ONE_HOUR_IN_SECONDS = 60 * 60;
-export const ONE_DAY_IN_SECONDS = 60 * 60 * 24;
-export const ONE_WEEK_IN_SECONDS = 60 * 60 * 24 * 7;
+export const ONE_HOUR_IN_SECONDS = ONE_MINUTE_IN_SECONDS * 60;
+export const ONE_DAY_IN_SECONDS = ONE_HOUR_IN_SECONDS * 24;
+export const ONE_WEEK_IN_SECONDS = ONE_DAY_IN_SECONDS * 7;
 
 // Utils
 
 export const cache = {
+  /**
+   * Gets the value stored in Redis and parses it as JSON. If the key does not
+   * exist, it will return null.
+   *
+   * @param key - Key to retrieve the value from.
+   */
   async get<T>(key: string) {
     const value = await redis.get(key);
 
@@ -45,6 +51,14 @@ export const cache = {
     return JSON.parse(value) as T;
   },
 
+  /**
+   * Stringifies the value and stores it in Redis. If an expiration time is
+   * provided, the key will expire after that time.
+   *
+   * @param key - Key to store the value in.
+   * @param data - JSON data to store in Redis.
+   * @param expires - Time (in seconds) for the key to expire.
+   */
   async set<T>(key: string, data: T, expires?: number) {
     const value = JSON.stringify(data);
 
