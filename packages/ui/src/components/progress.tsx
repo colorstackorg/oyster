bb5c3534b89c0582cc7d2d6f12076648bc136cf9
Rami Abdou <ramiabdou98@gmail.com>
2024-08-13 11:22:33 -0700

    progress bar

--- packages/ui/src/components/progress.tsx
+++ packages/ui/src/components/progress.tsx
@@ -1,15 +1,59 @@
 import { Indicator, type ProgressProps, Root } from '@radix-ui/react-progress';
+import { useEffect, useState } from 'react';
 
-export function Progress({ value, ...props }: ProgressProps) {
+type ProgressOptions = {
+  coolingFactor?: number;
+};
+
+const defaultOptions: Required<ProgressOptions> = {
+  coolingFactor: 0.03,
+};
+
+export function useProgress(options: ProgressOptions = {}) {
+  const { coolingFactor } = {
+    ...defaultOptions,
+    ...options,
+  };
+
+  const [progress, setProgress] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setProgress((value) => {
+        // Increment the progress by x% of the remaining value. This is
+        // effectively a "cooling" effect that slows down more as it approaches
+        // 100%.
+        const increment = (100 - value) * coolingFactor;
+
+        return value + increment;
+      });
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  return progress;
+}
+
+export function Progress({ max = 100, value, ...props }: ProgressProps) {
+  const translateX = 100 - (value || 0);
+
   return (
     <Root
       className="relative h-2 w-full overflow-hidden rounded-full bg-gray-100"
+      max={max}
       value={value}
       {...props}
     >
       <Indicator
         className="h-full w-full bg-primary transition-transform delay-700"
-        style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
+        style={{
+          // We'll effectively "push" the indicator to the left by the inverse,
+          // but it doesn't show because of the "overflow-hidden" on the parent.
+          transform: `translateX(-${translateX}%)`,
+        }}
       />
     </Root>
   );
