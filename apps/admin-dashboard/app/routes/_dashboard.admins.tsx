f9e3368141a4c43bac85ecad0708c6fa04acb087
Wilfredo <58885172+wflore19@users.noreply.github.com>
2024-08-19 20:47:34 -0400

    feat: ability to remove admins ❌ (#408)

--- apps/admin-dashboard/app/routes/_dashboard.admins.tsx
+++ apps/admin-dashboard/app/routes/_dashboard.admins.tsx
@@ -1,23 +1,58 @@
 import { json, type LoaderFunctionArgs } from '@remix-run/node';
 import { Outlet, useLoaderData } from '@remix-run/react';
 
-import { listAdmins } from '@oyster/core/admins';
+import {
+  doesAdminHavePermission,
+  getAdmin,
+  listAdmins,
+} from '@oyster/core/admins';
+import { type AdminRole } from '@oyster/core/admins.types';
 import { AdminTable } from '@oyster/core/admins.ui';
 import { Dashboard } from '@oyster/ui';
 
 import { ensureUserAuthenticated } from '@/shared/session.server';
+import { user } from '@/shared/session.server';
 
 export async function loader({ request }: LoaderFunctionArgs) {
-  await ensureUserAuthenticated(request);
+  const session = await ensureUserAuthenticated(request);
 
-  const admins = await listAdmins({
-    select: [
-      'admins.firstName',
-      'admins.lastName',
-      'admins.email',
-      'admins.id',
-      'admins.role',
-    ],
+  const [admin, _admins] = await Promise.all([
+    getAdmin({
+      select: ['admins.id', 'admins.role'],
+      where: { id: user(session) },
+    }),
+    listAdmins({
+      select: [
+        'admins.deletedAt',
+        'admins.firstName',
+        'admins.lastName',
+        'admins.email',
+        'admins.id',
+        'admins.role',
+      ],
+    }),
+  ]);
+
+  if (!admin) {
+    throw new Response(null, { status: 404 });
+  }
+
+  const admins = _admins.map(({ deletedAt, ...row }) => {
+    return {
+      ...row,
+
+      // Admins can't delete themselves nor can they delete other admins with
+      // a higher role.
+      canRemove:
+        !deletedAt &&
+        row.id !== admin.id &&
+        doesAdminHavePermission({
+          minimumRole: row.role as AdminRole,
+          role: admin.role as AdminRole,
+        }),
+
+      isDeleted: !!deletedAt,
+    };
   });
 
   return json({
