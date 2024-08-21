da7ee1e5af2fe31976dd3f7593e28c5e183c66a4
Rami Abdou <ramiabdou98@gmail.com>
2024-08-21 15:28:59 -0700

    feed progress

--- packages/core/src/api.ts
+++ packages/core/src/api.ts
@@ -18,7 +18,7 @@ export { loginWithOAuth } from './modules/authentication/use-cases/login-with-oa
 export { educationWorker } from './modules/education/education.worker';
 export { workExperienceWorker } from './modules/employment/employment.worker';
 export { eventWorker } from './modules/event/event.worker';
-export { feedWorker } from './modules/feed/feed.core';
+export { feedWorker } from './modules/feed/feed';
 export { gamificationWorker } from './modules/gamification/gamification.core';
 export { saveGoogleDriveCredentials } from './modules/google-drive';
 export { emailMarketingWorker } from './modules/mailchimp/email-marketing.worker';
