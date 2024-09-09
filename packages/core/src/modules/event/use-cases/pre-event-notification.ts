1588bc34b7f9251b3526c3cd44f5ccf9c32be587
rafa1510 <34660086+rafa1510@users.noreply.github.com>
2024-09-09 18:23:09 -0400

    create a preEventNotificationJob when an event is created
    this job will execute sendPreEventNotification() 2 days before an event

--- packages/core/src/modules/event/use-cases/pre-event-notification.ts
+++ packages/core/src/modules/event/use-cases/pre-event-notification.ts
@@ -1,6 +1,8 @@
+import dayjs from 'dayjs';
 import { match } from 'ts-pattern';
 
 import { PreEventNotificationBullJob } from '@/infrastructure/bull/bull.types';
+import { job } from '@/infrastructure/bull/use-cases/job';
 import { registerWorker } from '@/infrastructure/bull/use-cases/register-worker';
 // import { sendPreEventNotification } from './send-pre-event-notification';
 
@@ -16,3 +18,31 @@ export const preEventNotificationWorker = registerWorker(
       .exhaustive();
   }
 );
+
+type createPreEventNotificationJobInput = {
+  eventID: string;
+  startDate: dayjs.Dayjs;
+  timezone: string;
+};
+
+export async function createPreEventNotificationJob({
+  eventID,
+  startDate,
+  timezone,
+}: createPreEventNotificationJobInput) {
+  const twoDaysBeforeEvent = dayjs.tz(startDate, timezone).subtract(2, 'day');
+  const today = dayjs.tz(dayjs(), timezone);
+
+  // If event is two days from now, create a job to send pre-event notification
+  if (today.isBefore(twoDaysBeforeEvent)) {
+    const delay = twoDaysBeforeEvent.diff(today);
+
+    const scheduledJob = job(
+      'pre_event_notification.created',
+      { eventID },
+      { delay }
+    );
+  }
+
+  // TODO: Add job ID to event in database
+}
