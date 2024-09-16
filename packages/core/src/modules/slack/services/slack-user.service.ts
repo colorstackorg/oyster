3d3460a9479ffd2ee91cc41007a3de0fc4287e60
Sarah Vasquez <savasquez@ucdavis.edu>
2024-09-16 00:02:29 -0700

    added function to filter members with both slack and member directory profile

--- packages/core/src/modules/slack/services/slack-user.service.ts
+++ packages/core/src/modules/slack/services/slack-user.service.ts
@@ -1,5 +1,8 @@
 import { slack } from '@/modules/slack/instances';
 import { ENV } from '@/shared/env';
+import { db } from '@oyster/db';
+import { generatePath } from '@remix-run/react';
+//import { Route } from '@/shared/constants';
 
 /**
  * @see https://api.slack.com/methods/users.lookupByEmail
@@ -34,4 +37,25 @@ export async function updateSlackEmail(id: string, email: string) {
     token: ENV.SLACK_ADMIN_TOKEN,
     user: id,
   });
+}
+
+
+/**
+ * Retrieve all members with both Slack and Member Directory Profiles
+ */
+
+export async function getMembersWithProfiles() {
+  try {
+   const members = await db
+    .selectFrom('students') // Specify the table you're selecting from
+    .select(['id', 'email', 'joined_slack_at', 'joined_member_directory_at'] as any)
+    .where('joined_slack_at', 'is not', null)
+    .where('joined_member_directory_at', 'is not', null)
+    .execute();
+
+    console.log('Retrieved members:', members);
+  } catch (error) {
+    console.error('Error retrieving members:', error);
+  }
+
 }
