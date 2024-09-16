77568db7f3a2270dc90daec25074cce8490c81b8
Rami Abdou <ramiabdou98@gmail.com>
2024-09-16 10:16:50 -0700

    post ephemeral message

--- packages/core/src/modules/notification/use-cases/send-slack-notification.ts
+++ packages/core/src/modules/notification/use-cases/send-slack-notification.ts
@@ -10,6 +10,14 @@ type SendNotificationInput =
       workspace: 'regular';
     }
   | {
+      channel: string;
+      ephemeral: true;
+      message: string;
+      threadId?: string;
+      workspace: 'regular';
+      userId: string;
+    }
+  | {
       channel?: string;
       message: string;
       threadId?: string;
@@ -27,9 +35,18 @@ export async function sendSlackNotification(input: SendNotificationInput) {
 
   const channel = input.channel || ENV.INTERNAL_SLACK_NOTIFICATIONS_CHANNEL_ID;
 
-  await client.chat.postMessage({
-    channel,
-    text: input.message,
-    thread_ts: input.threadId,
-  });
+  if ('ephemeral' in input && input.ephemeral) {
+    await client.chat.postEphemeral({
+      channel,
+      text: input.message,
+      thread_ts: input.threadId,
+      user: input.userId,
+    });
+  } else {
+    await client.chat.postMessage({
+      channel,
+      text: input.message,
+      thread_ts: input.threadId,
+    });
+  }
 }
