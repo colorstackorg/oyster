75db7924650f7d443ce775f483de2f7130b720b2
imnotrafa <abreurafa22@gmail.com>
2024-09-25 11:59:11 -0400

    Implemented sendEmail for every single unregistered student for given event

--- packages/core/src/modules/event/use-cases/event-reminder.ts
+++ packages/core/src/modules/event/use-cases/event-reminder.ts
@@ -2,9 +2,7 @@ import { db } from '@oyster/db';
 import { type Event } from '@oyster/types';
 import { listEventAttendees } from '@/member-profile.server';
 import { sendEmail } from '@/modules/notification/use-cases/send-email';
-import { Email } from '../../../../../types/src/domain/types';
-import { send } from 'process';
-
+import { StudentActivatedEmail } from '@oyster/email-templates';
 export async function eventReminder(input: Event) {
   //selects students who are undergrad
   let RegisteredStudentIDs: string[] = [];
@@ -20,16 +18,23 @@ export async function eventReminder(input: Event) {
       RegisteredStudentIDs.push(attendee.studentId);
     }
   });
-  // filter down to just the undergrads that are not register for the event
+  //filter down to just the undergrads that are not register for the event
   const unregisteredStudents = await db
     .selectFrom('students')
-    .select(['students.email'])
+    .select(['students.email', 'students.firstName'])
     .where('educationLevel', '=', 'undergraduate')
     .where('id', 'not in', RegisteredStudentIDs)
     .execute();
-  const unregisteredStudentsEmailList = unregisteredStudents.map(
-    (studentEmail) => studentEmail.email
-  );
-  //list of unregistered student emails
-  return unregisteredStudentsEmailList;
+
+  //Must change the name of the email template being used based on the prupose of the function
+  //TODO: create an email tenplate for event reminders
+  unregisteredStudents.forEach((student) => {
+    sendEmail({
+      name: 'application-accepted',
+      to: student.email,
+      data: {
+        firstName: student.firstName,
+      },
+    });
+  });
 }
