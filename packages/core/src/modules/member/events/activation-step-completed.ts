f168cf9d7378d1de708f901fcbc0ad5d2b84e840
Rami Abdou <38056800+ramiAbdou@users.noreply.github.com>
2024-09-30 08:57:00 -0700

    chore: remove all swag up code 🚮 (#531)

--- packages/core/src/modules/member/events/activation-step-completed.ts
+++ packages/core/src/modules/member/events/activation-step-completed.ts
@@ -230,7 +230,7 @@ async function sendProgressNotification({
 
       You've completed all of your activation requirements, which means...you are now an *activated* ColorStack member.
 
-      You can now claim your free ColorStack merch <https://app.colorstack.io/home/claim-swag-pack|*here*>! 🎁
+      Look out for an email that includes a GIFT CARD to the <https://colorstackmerch.org|*ColorStack Merch Store*>! 🎁
     `;
   } else {
     message = dedent`
