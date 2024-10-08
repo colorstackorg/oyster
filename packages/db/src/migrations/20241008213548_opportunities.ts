f05b165cf9d1f765308f7b4ea039b8d46b731dde
Rami Abdou <ramiabdou98@gmail.com>
2024-10-08 14:45:17 -0700

    opportunities migration

---
+++ packages/db/src/migrations/20241008213548_opportunities.ts
@@ -0,0 +1,57 @@
+import { type Kysely, sql } from 'kysely';
+
+export async function up(db: Kysely<any>) {
+  await db.schema
+    .createTable('opportunities')
+    .addColumn('created_at', 'timestamptz', (column) => {
+      return column.notNull().defaultTo(sql`now()`);
+    })
+    .addColumn('description', 'text')
+    .addColumn('expires_at', 'timestamptz', (column) => {
+      return column.notNull();
+    })
+    .addColumn('id', 'text', (column) => {
+      return column.primaryKey();
+    })
+    .addColumn('posted_by_id', 'text', (column) => {
+      return column.references('students.id').onDelete('set null');
+    })
+    .addColumn('posted_by_slack_id', 'text', (column) => {
+      return column.notNull();
+    })
+    .addColumn('slack_channel_id', 'text', (column) => {
+      return column.notNull();
+    })
+    .addColumn('slack_message_id', 'text', (column) => {
+      return column.notNull();
+    })
+    .addColumn('title', 'text', (column) => {
+      return column.notNull();
+    })
+    .addColumn('type', 'text', (column) => {
+      return column.notNull();
+    })
+    .execute();
+
+  await db.schema
+    .createTable('opportunity_companies')
+    .addColumn('company_id', 'text', (column) => {
+      return column.notNull().references('companies.id');
+    })
+    .addColumn('opportunity_id', 'text', (column) => {
+      return column
+        .notNull()
+        .references('opportunities.id')
+        .onDelete('cascade');
+    })
+    .addPrimaryKeyConstraint('opportunity_companies_pkey', [
+      'company_id',
+      'opportunity_id',
+    ])
+    .execute();
+}
+
+export async function down(db: Kysely<any>) {
+  await db.schema.dropTable('opportunity_companies').execute();
+  await db.schema.dropTable('opportunities').execute();
+}
