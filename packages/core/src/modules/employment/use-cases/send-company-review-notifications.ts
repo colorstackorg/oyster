406ea6ef632559422ec4d39e07a3a74ff6041fd4
rafa1510 <34660086+rafa1510@users.noreply.github.com>
2024-10-22 22:11:21 -0400

    refactor: ensure work experience end date is within last and current month

--- packages/core/src/modules/employment/use-cases/send-company-review-notifications.ts
+++ packages/core/src/modules/employment/use-cases/send-company-review-notifications.ts
@@ -6,14 +6,20 @@ import { job } from '@/infrastructure/bull/use-cases/job';
 import { ENV } from '@/shared/env';
 
 /**
- * This is a weekly job that runs and finds all students that have had a work
- * experience end in the last week. Then, it has the ColorStack bot send a DM
+ * This is a monthly job that runs and finds all students that have had a work
+ * experience end in the last month. Then, it has the ColorStack bot send a DM
  * to all these students suggesting that they add a review of their experience.
  */
 export async function sendCompanyReviewNotifications() {
-  const oneMonthAgo = new Date();
+  const now = new Date();
 
-  oneMonthAgo.setDate(oneMonthAgo.getDate() - 32);
+  const firstDayOfPreviousMonth = new Date(
+    Date.UTC(now.getUTCFullYear(), now.getUTCMonth() - 1, 1)
+  );
+
+  const firstDayOfCurrentMonth = new Date(
+    Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), 1)
+  );
 
   const results = await db
     .selectFrom('workExperiences')
@@ -24,7 +30,8 @@ export async function sendCompanyReviewNotifications() {
       'students.firstName as studentName',
       'students.slackId as studentSlackId',
     ])
-    .where('endDate', '>=', oneMonthAgo)
+    .where('endDate', '>=', firstDayOfPreviousMonth)
+    .where('endDate', '<', firstDayOfCurrentMonth)
     .where('endDate', 'is not', null)
     .execute();
 
