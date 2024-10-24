5d03f86290ce204ee0b36f7b8e294d2f38cbce3b
Rami Abdou <ramiabdou98@gmail.com>
2024-10-24 06:11:03 -0700

    add boilerplate

--- apps/member-profile/app/routes/_profile.tsx
+++ apps/member-profile/app/routes/_profile.tsx
@@ -6,12 +6,14 @@ import {
   BookOpen,
   Briefcase,
   Calendar,
+  DollarSign,
   FileText,
   Folder,
   Home,
   User,
 } from 'react-feather';
 
+import { isFeatureFlagEnabled } from '@oyster/core/member-profile/server';
 import { getResumeBook } from '@oyster/core/resumes';
 import { Dashboard, Divider } from '@oyster/ui';
 
@@ -21,21 +23,26 @@ import { ensureUserAuthenticated } from '@/shared/session.server';
 export async function loader({ request }: LoaderFunctionArgs) {
   await ensureUserAuthenticated(request);
 
-  const resumeBook = await getResumeBook({
-    select: ['resumeBooks.id'],
-    where: {
-      hidden: false,
-      status: 'active',
-    },
-  });
+  const [isOpportunitiesEnabled, resumeBook] = await Promise.all([
+    isFeatureFlagEnabled('opportunities'),
+
+    getResumeBook({
+      select: ['resumeBooks.id'],
+      where: {
+        hidden: false,
+        status: 'active',
+      },
+    }),
+  ]);
 
   return json({
+    isOpportunitiesEnabled,
     resumeBook,
   });
 }
 
 export default function ProfileLayout() {
-  const { resumeBook } = useLoaderData<typeof loader>();
+  const { isOpportunitiesEnabled, resumeBook } = useLoaderData<typeof loader>();
 
   return (
     <Dashboard>
@@ -51,7 +58,6 @@ export default function ProfileLayout() {
               <>
                 <Dashboard.NavigationLink
                   icon={<Book />}
-                  isNew
                   label="Resume Book"
                   pathname={generatePath(Route['/resume-books/:id'], {
                     id: resumeBook.id,
@@ -74,16 +80,25 @@ export default function ProfileLayout() {
               pathname={Route['/directory']}
               prefetch="intent"
             />
-            <Dashboard.NavigationLink
-              icon={<BookOpen />}
-              label="Resources"
-              pathname={Route['/resources']}
-              prefetch="intent"
-            />
+            {isOpportunitiesEnabled && (
+              <Dashboard.NavigationLink
+                icon={<DollarSign />}
+                isNew
+                label="Opportunities"
+                pathname={Route['/opportunities']}
+                prefetch="intent"
+              />
+            )}
             <Dashboard.NavigationLink
               icon={<Briefcase />}
               label="Companies"
               pathname={Route['/companies']}
+              prefetch="intent"
+            />
+            <Dashboard.NavigationLink
+              icon={<BookOpen />}
+              label="Resources"
+              pathname={Route['/resources']}
               prefetch="intent"
             />
             <Dashboard.NavigationLink
@@ -100,7 +115,6 @@ export default function ProfileLayout() {
             />
             <Dashboard.NavigationLink
               icon={<FileText />}
-              isNew
               label="Resume Review"
               pathname={Route['/resume/review']}
               prefetch="intent"
