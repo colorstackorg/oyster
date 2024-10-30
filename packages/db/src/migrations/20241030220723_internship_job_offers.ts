36b6d7dacdc20eeb68970520086f69ecc785a1a2
tomas-salgado <tas268@cornell.edu>
2024-10-30 18:52:18 -0400

    add internship_job_offers and full_time_job_offers db tables

---
+++ packages/db/src/migrations/20241030220723_internship_job_offers.ts
@@ -0,0 +1,64 @@
+import { type Kysely, sql } from 'kysely';
+
+export async function up(db: Kysely<any>) {
+  await db.schema
+    .createTable('internship_job_offers')
+    .addColumn('id', 'text', (cb) => cb.primaryKey())
+    .addColumn('created_at', 'timestamptz', (cb) =>
+      cb.notNull().defaultTo(sql`now()`)
+    )
+    .addColumn('hourly_salary', 'integer')
+    .addColumn('monthly_salary', 'integer')
+    .addColumn('company_id', 'text', (cb) => {
+      return cb.references('companies.id');
+    })
+    .addColumn('other_company', 'text')
+    .addColumn('start_date', 'date', (cb) => cb.notNull())
+    .addColumn('updated_at', 'timestamptz', (cb) =>
+      cb.notNull().defaultTo(sql`now()`)
+    )
+    .addColumn('location', 'text')
+    .addColumn('location_coordinates', sql`point`)
+    .addColumn('location_type', 'text', (cb) => cb.notNull())
+    .addColumn('role', 'text', (cb) => cb.notNull())
+    .addColumn('equity_or_stock_text', 'text')
+    .addColumn('bonus', 'integer')
+    .addColumn('bonus_text', 'text')
+    .addColumn('relocation', 'integer')
+    .addColumn('relocation_text', 'text')
+    .addColumn('benefits', 'text')
+    .addColumn('is_negotiated', 'boolean')
+    .addColumn('is_accepted', 'boolean')
+    .addColumn('decision_reason', 'text')
+    .addColumn('posted_by', 'text', (cb) => {
+      return cb.references('students.id').notNull();
+    })
+    .addColumn('slack_channel_id', 'text')
+    .addColumn('slack_message_id', 'text')
+    .addForeignKeyConstraint(
+      'internship_job_offers_slack_message_fkey',
+      ['slack_channel_id', 'slack_message_id'],
+      'slack_messages',
+      ['channel_id', 'id'],
+      (constraint) => constraint.onDelete('cascade')
+    )
+    .execute();
+
+  // This will help when querying job offers by company.
+  await db.schema
+    .createIndex('internship_job_offers_company_id_idx')
+    .on('internship_job_offers')
+    .column('company_id')
+    .execute();
+
+  // This will help when querying job offers by the student who posted them.
+  await db.schema
+    .createIndex('internship_job_offers_posted_by_idx')
+    .on('internship_job_offers')
+    .column('posted_by')
+    .execute();
+}
+
+export async function down(db: Kysely<any>) {
+  await db.schema.dropTable('internship_job_offers').execute();
+}
---
+++ packages/db/src/migrations/20241030221007_full_time_job_offers.ts
@@ -0,0 +1,64 @@
+import { type Kysely, sql } from 'kysely';
+
+export async function up(db: Kysely<any>) {
+  await db.schema
+    .createTable('full_time_job_offers')
+    .addColumn('id', 'text', (cb) => cb.primaryKey())
+    .addColumn('created_at', 'timestamptz', (cb) =>
+      cb.notNull().defaultTo(sql`now()`)
+    )
+    .addColumn('base_salary', 'integer')
+    .addColumn('bonus', 'integer')
+    .addColumn('bonus_text', 'text')
+    .addColumn('company_id', 'text', (cb) => {
+      return cb.references('companies.id');
+    })
+    .addColumn('other_company', 'text')
+    .addColumn('start_date', 'date', (cb) => cb.notNull())
+    .addColumn('stock_per_year', 'integer')
+    .addColumn('equity_or_stock_text', 'text')
+    .addColumn('updated_at', 'timestamptz', (cb) =>
+      cb.notNull().defaultTo(sql`now()`)
+    )
+    .addColumn('location', 'text')
+    .addColumn('location_coordinates', sql`point`)
+    .addColumn('location_type', 'text', (cb) => cb.notNull())
+    .addColumn('role', 'text', (cb) => cb.notNull())
+    .addColumn('total_compensation_text', 'text')
+    .addColumn('is_negotiated', 'boolean', (cb) =>
+      cb.notNull().defaultTo(false)
+    )
+    .addColumn('is_accepted', 'boolean', (cb) => cb.notNull().defaultTo(false))
+    .addColumn('accepted_reason', 'text')
+    .addColumn('posted_by', 'text', (cb) => {
+      return cb.references('students.id').notNull();
+    })
+    .addColumn('slack_channel_id', 'text', (cb) => cb.notNull())
+    .addColumn('slack_message_id', 'text', (cb) => cb.notNull())
+    .addForeignKeyConstraint(
+      'full_time_job_offers_slack_message_fkey',
+      ['slack_channel_id', 'slack_message_id'],
+      'slack_messages',
+      ['channel_id', 'id'],
+      (constraint) => constraint.onDelete('cascade')
+    )
+    .execute();
+
+  // This will help when querying job offers by company.
+  await db.schema
+    .createIndex('full_time_job_offers_company_id_idx')
+    .on('full_time_job_offers')
+    .column('company_id')
+    .execute();
+
+  // This will help when querying job offers by the student who posted them.
+  await db.schema
+    .createIndex('full_time_job_offers_posted_by_idx')
+    .on('full_time_job_offers')
+    .column('posted_by')
+    .execute();
+}
+
+export async function down(db: Kysely<any>) {
+  await db.schema.dropTable('full_time_job_offers').execute();
+}
