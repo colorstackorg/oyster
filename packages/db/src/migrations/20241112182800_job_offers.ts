2f303de71c42ae2f1548aaf3239e5cba2ce9a671
Rami Abdou <ramiabdou98@gmail.com>
2024-11-12 10:42:11 -0800

    sort

--- packages/db/src/migrations/20241112182800_job_offers.ts
+++ packages/db/src/migrations/20241112182800_job_offers.ts
@@ -3,109 +3,116 @@ import { type Kysely, sql } from 'kysely';
 export async function up(db: Kysely<any>) {
   await db.schema
     .createTable('internship_job_offers')
-    .addColumn('id', 'text', (cb) => {
-      return cb.primaryKey();
+    .addColumn('additional_notes', 'text')
+    .addColumn('benefits', 'text')
+    .addColumn('company_id', 'text', (column) => {
+      return column.references('companies.id');
     })
-    .addColumn('created_at', 'timestamptz', (cb) =>
-      cb.notNull().defaultTo(sql`now()`)
-    )
-    .addColumn('updated_at', 'timestamptz', (cb) =>
-      cb.notNull().defaultTo(sql`now()`)
-    )
-    .addColumn('role', 'text')
+    .addColumn('created_at', 'timestamptz', (column) => {
+      return column.notNull().defaultTo(sql`now()`);
+    })
     .addColumn('hourly_rate', 'integer')
-    .addColumn('monthly_rate', 'integer')
+    .addColumn('id', 'text', (column) => {
+      return column.primaryKey();
+    })
     .addColumn('location', 'text')
     .addColumn('location_type', 'text')
-    .addColumn('relocation_text', 'text')
-    .addColumn('benefits', 'text')
-    .addColumn('years_of_experience', 'text')
+    .addColumn('monthly_rate', 'integer')
     .addColumn('negotiated_text', 'text')
-    .addColumn('additional_notes', 'text')
-    .addColumn('company_id', 'text', (cb) => {
-      return cb.references('companies.id');
-    })
     .addColumn('other_company', 'text')
     .addColumn('posted_by', 'text', (column) => {
       return column.references('students.id').onDelete('set null');
     })
+    .addColumn('relocation_text', 'text')
+    .addColumn('role', 'text')
     .addColumn('slack_channel_id', 'text')
     .addColumn('slack_message_id', 'text')
+    .addColumn('updated_at', 'timestamptz', (column) => {
+      return column.notNull().defaultTo(sql`now()`);
+    })
+    .addColumn('years_of_experience', 'text')
     .addForeignKeyConstraint(
       'internship_job_offers_slack_message_fkey',
       ['slack_channel_id', 'slack_message_id'],
       'slack_messages',
       ['channel_id', 'id'],
-      (constraint) => constraint.onDelete('cascade')
+      (constraint) => {
+        return constraint.onDelete('cascade');
+      }
     )
     .execute();
 
   await db.schema
     .createTable('full_time_job_offers')
-    .addColumn('id', 'text', (cb) => {
-      return cb.primaryKey();
-    })
-    .addColumn('created_at', 'timestamptz', (cb) =>
-      cb.notNull().defaultTo(sql`now()`)
-    )
-    .addColumn('updated_at', 'timestamptz', (cb) =>
-      cb.notNull().defaultTo(sql`now()`)
-    )
-    .addColumn('role', 'text')
+    .addColumn('additional_notes', 'text')
     .addColumn('base_salary', 'integer')
+    .addColumn('bonus', 'integer')
+    .addColumn('company_id', 'text', (column) => {
+      return column.references('companies.id');
+    })
+    .addColumn('created_at', 'timestamptz', (column) => {
+      return column.notNull().defaultTo(sql`now()`);
+    })
     .addColumn('hourly_rate', 'integer')
+    .addColumn('id', 'text', (column) => {
+      return column.primaryKey();
+    })
     .addColumn('location', 'text')
     .addColumn('location_type', 'text')
-    .addColumn('stock_per_year', 'integer')
-    .addColumn('bonus', 'integer')
-    .addColumn('total_compensation', 'integer')
-    .addColumn('performance_bonus_text', 'text')
-    .addColumn('sign_on_bonus_text', 'text')
-    .addColumn('relocation_text', 'text')
-    .addColumn('benefits', 'text')
-    .addColumn('years_of_experience', 'text')
     .addColumn('negotiated_text', 'text')
-    .addColumn('additional_notes', 'text')
-    .addColumn('company_id', 'text', (cb) => {
-      return cb.references('companies.id');
-    })
     .addColumn('other_company', 'text')
+    .addColumn('performance_bonus_text', 'text')
     .addColumn('posted_by', 'text', (column) => {
       return column.references('students.id').onDelete('set null');
     })
-    .addColumn('slack_channel_id', 'text', (cb) => cb.notNull())
-    .addColumn('slack_message_id', 'text', (cb) => cb.notNull())
+    .addColumn('relocation_text', 'text')
+    .addColumn('role', 'text')
+    .addColumn('sign_on_bonus_text', 'text')
+    .addColumn('slack_channel_id', 'text', (column) => {
+      return column.notNull();
+    })
+    .addColumn('slack_message_id', 'text', (column) => {
+      return column.notNull();
+    })
+    .addColumn('stock_per_year', 'integer')
+    .addColumn('total_compensation', 'integer')
+    .addColumn('updated_at', 'timestamptz', (column) => {
+      return column.notNull().defaultTo(sql`now()`);
+    })
+    .addColumn('years_of_experience', 'text')
     .addForeignKeyConstraint(
       'full_time_job_offers_slack_message_fkey',
       ['slack_channel_id', 'slack_message_id'],
       'slack_messages',
       ['channel_id', 'id'],
-      (constraint) => constraint.onDelete('cascade')
+      (constraint) => {
+        return constraint.onDelete('cascade');
+      }
     )
     .execute();
 
-  // This will help when querying job offers by company.
+  // Will help when querying internship job offers by company.
   await db.schema
     .createIndex('internship_job_offers_company_id_idx')
     .on('internship_job_offers')
     .column('company_id')
     .execute();
 
-  // This will help when querying job offers by the student who posted them.
+  // Will help when querying internship job offers by who posted them.
   await db.schema
     .createIndex('internship_job_offers_posted_by_idx')
     .on('internship_job_offers')
     .column('posted_by')
     .execute();
 
-  // This will help when querying job offers by company.
+  // Will help when querying FT job offers by company.
   await db.schema
     .createIndex('full_time_job_offers_company_id_idx')
     .on('full_time_job_offers')
     .column('company_id')
     .execute();
 
-  // This will help when querying job offers by the student who posted them.
+  // Will help when querying FT job offers by who posted them.
   await db.schema
     .createIndex('full_time_job_offers_posted_by_idx')
     .on('full_time_job_offers')
