f9039a7b33985020de18d592cff81a457875d287
Rami Abdou <ramiabdou98@gmail.com>
2024-11-11 20:50:01 -0800

    add documentation

--- packages/db/src/use-cases/create-database-connection.ts
+++ packages/db/src/use-cases/create-database-connection.ts
@@ -11,13 +11,18 @@ export function createDatabaseConnection(url: string = DATABASE_URL) {
     );
   }
 
+  // In Railway (production), the `DATABASE_URL` is an internal URL that is
+  // part of a private network. However in the build phase, that private
+  // network is not available, so we need to use the public URL instead (simply)
+  // to generate the database types and run the migrations. The public URL is
+  // available via `DATABASE_PUBLIC_URL` in the build environment variables.
+  const connectionString =
+    process.env.IS_RAILWAY_BUILD === 'true'
+      ? process.env.DATABASE_PUBLIC_URL
+      : url;
+
   const dialect = new PostgresDialect({
-    pool: new pg.Pool({
-      connectionString:
-        process.env.IS_RAILWAY_BUILD === 'true'
-          ? process.env.DATABASE_PUBLIC_URL
-          : url,
-    }),
+    pool: new pg.Pool({ connectionString }),
   });
 
   const db = new Kysely<DB>({
