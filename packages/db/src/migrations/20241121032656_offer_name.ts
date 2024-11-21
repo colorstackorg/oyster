c3026b08f96774c519d049895d712e1fadb4fd6b
Rami Abdou <ramiabdou98@gmail.com>
2024-11-20 20:51:53 -0800

    role not null

--- packages/db/src/migrations/20241121032656_offer_name.ts
+++ packages/db/src/migrations/20241121032656_offer_name.ts
@@ -10,9 +10,37 @@ export async function up(db: Kysely<any>) {
     .alterTable('internship_job_offers')
     .renameTo('internship_offers')
     .execute();
+
+  await db.schema
+    .alterTable('full_time_offers')
+    .alterColumn('role', (column) => {
+      return column.setNotNull();
+    })
+    .execute();
+
+  await db.schema
+    .alterTable('internship_offers')
+    .alterColumn('role', (column) => {
+      return column.setNotNull();
+    })
+    .execute();
 }
 
 export async function down(db: Kysely<any>) {
+  await db.schema
+    .alterTable('internship_offers')
+    .alterColumn('role', (column) => {
+      return column.dropNotNull();
+    })
+    .execute();
+
+  await db.schema
+    .alterTable('full_time_offers')
+    .alterColumn('role', (column) => {
+      return column.dropNotNull();
+    })
+    .execute();
+
   await db.schema
     .alterTable('full_time_offers')
     .renameTo('full_time_job_offers')
