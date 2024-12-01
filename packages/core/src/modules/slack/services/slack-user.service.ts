f5e93e98e736d7de8a36980f3d5b6772a85bd6e3
Tomas Salgado <91388965+tomas-salgado@users.noreply.github.com>
2024-12-01 14:26:53 -0600

    update import order

--- packages/core/src/modules/slack/services/slack-user.service.ts
+++ packages/core/src/modules/slack/services/slack-user.service.ts
@@ -1,7 +1,9 @@
+import { generatePath } from '@remix-run/react';
+
+import { db } from '@oyster/db';
+
 import { slack } from '@/modules/slack/instances';
 import { ENV } from '@/shared/env';
-import { db } from '@oyster/db';
-import {generatePath} from '@remix-run/react'; 
 
 /**
  * @see https://api.slack.com/methods/users.lookupByEmail
@@ -38,7 +40,6 @@ export async function updateSlackEmail(id: string, email: string) {
   });
 }
 
-
 /**
  * Retrieve all members with both Slack and Member Directory Profiles
  */
@@ -46,67 +47,75 @@ export async function updateSlackEmail(id: string, email: string) {
 export async function getMembersWithProfilesandSlack() {
   try {
     const members = await db
-    .selectFrom('students')
-    .select(['id','email','joinedSlackAt','joinedMemberDirectoryAt'])
-    .where('joinedSlackAt', 'is not', null)
-    .where('joinedMemberDirectoryAt', 'is not', null)
-    .execute();
+      .selectFrom('students')
+      .select(['id', 'email', 'joinedSlackAt', 'joinedMemberDirectoryAt'])
+      .where('joinedSlackAt', 'is not', null)
+      .where('joinedMemberDirectoryAt', 'is not', null)
+      .execute();
 
-    return members
+    return members;
   } catch (error) {
-    console.error('Failed to retrieve members with profiles', error )
-    return []
-  }
+    console.error('Failed to retrieve members with profiles', error);
 
+    return [];
+  }
 }
 
-export async function setMemberProfileToSlackUserProfile(id: string, memberDirectoryURL: string) {
-    try {
-      const profile = {
-        fields: {
-          X123: { // unsure of what the field ID is called is it created?
-            value: memberDirectoryURL,
-            alt: '',
-          }
-        }
-      }
+export async function setMemberProfileToSlackUserProfile(
+  id: string,
+  memberDirectoryURL: string
+) {
+  try {
+    const profile = {
+      fields: {
+        X123: {
+          // unsure of what the field ID is called is it created?
+          value: memberDirectoryURL,
+          alt: '',
+        },
+      },
+    };
 
-      await slack.users.profile.set({
-        profile: JSON.stringify(profile), 
-        token: ENV.SLACK_ADMIN_TOKEN,
-        user: id,
-      })
+    await slack.users.profile.set({
+      profile: JSON.stringify(profile),
+      token: ENV.SLACK_ADMIN_TOKEN,
+      user: id,
+    });
 
-      console.log(`SuccessFully updated Slack Profile${id}`)
-    } catch (error) {
-      console.error(`Failed To Update Slack Profile ${id}`, error)
-    }
+    console.log(`SuccessFully updated Slack Profile${id}`);
+  } catch (error) {
+    console.error(`Failed To Update Slack Profile ${id}`, error);
+  }
 }
 
 export async function updateSlackProfilesWithMemberURLS() {
   const members = await getMembersWithProfilesandSlack();
 
   for (const member of members) {
-    const {id, email} = member;
+    const { id, email } = member;
 
-    const slackUser = await getSlackUserByEmail(email)
+    const slackUser = await getSlackUserByEmail(email);
 
     if (slackUser && slackUser.id) {
-      const baseURL = 'https://app.colorstack.io'
-      const memberDirectoryURL = `${baseURL}${generatePath(`/directory/:id`, {id})}`
+      const baseURL = 'https://app.colorstack.io';
+      const memberDirectoryURL = `${baseURL}${generatePath(`/directory/:id`, { id })}`;
 
-      await setMemberProfileToSlackUserProfile(slackUser.id, memberDirectoryURL);
+      await setMemberProfileToSlackUserProfile(
+        slackUser.id,
+        memberDirectoryURL
+      );
 
-      console.log(`Successfully updated Slack profile for user ${slackUser.id}`)
+      console.log(
+        `Successfully updated Slack profile for user ${slackUser.id}`
+      );
     } else {
-      console.log(`No Slack User Found for email: ${email}`)
+      console.log(`No Slack User Found for email: ${email}`);
     }
 
     await delay(10000);
-
   }
 }
 
 function delay(ms: number) {
-  return new Promise( resolve => setTimeout (resolve, ms))
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
