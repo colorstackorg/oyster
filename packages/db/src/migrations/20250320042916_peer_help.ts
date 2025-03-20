224f45be78da49fae3d36c3f0d5a778bdc2b0378
Rami Abdou <ramiabdou98@gmail.com>
2025-03-19 21:59:25 -0700

    migration

---
+++ packages/db/src/migrations/20250320042916_peer_help.ts
@@ -0,0 +1,41 @@
+import { type Kysely, sql } from 'kysely';
+
+export async function up(db: Kysely<any>) {
+  await db.schema
+    .createTable('help_requests')
+    .addColumn('created_at', 'timestamptz', (column) => {
+      return column.notNull().defaultTo(sql`now()`);
+    })
+    .addColumn('help_by', 'date', (column) => {
+      return column.notNull();
+    })
+    .addColumn('helpee_id', 'text', (column) => {
+      return column.notNull().references('students.id');
+    })
+    .addColumn('helper_id', 'text', (column) => {
+      return column.references('students.id');
+    })
+    .addColumn('id', 'text', (column) => {
+      return column.primaryKey();
+    })
+    .addColumn('message', 'text', (column) => {
+      return column.notNull();
+    })
+    .addColumn('status', 'text', (column) => {
+      return column.notNull();
+    })
+    .addColumn('summary', 'text', (column) => {
+      return column.notNull();
+    })
+    .addColumn('type', 'text', (column) => {
+      return column.notNull();
+    })
+    .addColumn('updated_at', 'timestamptz', (column) => {
+      return column.notNull().defaultTo(sql`now()`);
+    })
+    .execute();
+}
+
+export async function down(db: Kysely<any>) {
+  await db.schema.dropTable('help_requests').execute();
+}
