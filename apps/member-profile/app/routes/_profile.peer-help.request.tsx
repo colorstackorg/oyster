1290df10a89e52b233bffcb713c1924766c9a373
Rami Abdou <ramiabdou98@gmail.com>
2025-03-20 17:13:14 -0700

    progress

--- apps/member-profile/app/routes/_profile.peer-help.request.tsx
+++ apps/member-profile/app/routes/_profile.peer-help.request.tsx
@@ -1,25 +1,81 @@
-import { json, type LoaderFunctionArgs } from '@remix-run/node';
-import { Form, useSearchParams } from '@remix-run/react';
+import {
+  type ActionFunctionArgs,
+  json,
+  type LoaderFunctionArgs,
+  redirect,
+} from '@remix-run/node';
+import {
+  Form,
+  generatePath,
+  useActionData,
+  useSearchParams,
+} from '@remix-run/react';
 
+import { requestHelp, RequestHelpInput } from '@oyster/core/peer-help';
 import {
   Button,
   DatePicker,
   ErrorMessage,
   Field,
+  getErrors,
   Modal,
   Textarea,
+  validateForm,
 } from '@oyster/ui';
 import { Select } from '@oyster/ui';
 
 import { Route } from '@/shared/constants';
+import { commitSession, toast, user } from '@/shared/session.server';
+import { ensureUserAuthenticated } from '@/shared/session.server';
 
 export async function loader({ request }: LoaderFunctionArgs) {
+  await ensureUserAuthenticated(request);
+
   return json({});
+}
+
+export async function action({ request }: ActionFunctionArgs) {
+  const session = await ensureUserAuthenticated(request);
+
+  const { data, errors, ok } = await validateForm(
+    request,
+    RequestHelpInput.omit({ helpeeId: true })
+  );
+
+  if (!ok) {
+    return json({ errors }, { status: 400 });
+  }
+
+  const result = await requestHelp({
+    ...data,
+    helpeeId: user(session),
+  });
+
+  if (!result.ok) {
+    return json({ error: result.error }, { status: result.code });
+  }
+
+  toast(session, {
+    message: 'Request submitted!',
+  });
+
+  const url = new URL(request.url);
+
+  url.pathname = generatePath(Route['/peer-help/:id'], {
+    id: result.data.id,
+  });
+
+  return redirect(url.toString(), {
+    headers: {
+      'Set-Cookie': await commitSession(session),
+    },
+  });
 }
 
 // Page
 
 export default function RequestHelpModal() {
+  const { error, errors } = getErrors(useActionData<typeof action>());
   const [searchParams] = useSearchParams();
 
   return (
@@ -36,8 +92,8 @@ export default function RequestHelpModal() {
 
       <Form className="form" method="post">
         <Field
-          description="These are the types of help we currently support."
-          error=""
+          description="We currently only support these areas of need."
+          error={errors.type}
           label="What type of help do you need?"
           labelFor="type"
           required
@@ -51,7 +107,7 @@ export default function RequestHelpModal() {
 
         <Field
           description=""
-          error=""
+          error={errors.helpBy}
           label="When do you need help by?"
           labelFor="helpBy"
           required
@@ -68,15 +124,20 @@ export default function RequestHelpModal() {
 
         <Field
           description="This will help those who are looking to help find the right person."
-          error=""
+          error={errors.description}
           label="Please describe what you need help with in more detail."
           labelFor="description"
           required
         >
-          <Textarea id="description" name="description" />
+          <Textarea
+            id="description"
+            minLength={100}
+            name="description"
+            required
+          />
         </Field>
 
-        <ErrorMessage></ErrorMessage>
+        <ErrorMessage>{error}</ErrorMessage>
 
         <Button.Group>
           <Button.Submit>Request</Button.Submit>
