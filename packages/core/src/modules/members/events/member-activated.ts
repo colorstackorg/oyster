69f50eb763c54ff4c41636e002c9127ec28f42e0
Rami Abdou <ramiabdou98@gmail.com>
2025-06-30 08:49:13 -0700

    scrap shopify

--- packages/core/src/modules/members/events/member-activated.ts
+++ packages/core/src/modules/members/events/member-activated.ts
@@ -1,10 +1,7 @@
-import dayjs from 'dayjs';
-
 import { db } from '@oyster/db';
 
 import { job } from '@/infrastructure/bull';
 import { type GetBullJobData } from '@/infrastructure/bull.types';
-import { createGiftCard } from '@/modules/shopify';
 
 export async function onMemberActivated({
   studentId,
@@ -14,24 +11,6 @@ export async function onMemberActivated({
     .select(['email', 'firstName', 'id', 'lastName'])
     .where('id', '=', studentId)
     .executeTakeFirstOrThrow();
-
-  const giftCardResult = await createGiftCard({
-    expiresOn: dayjs().add(1, 'month').format('YYYY-MM-DD'),
-    initialValue: '50.00',
-    message:
-      'Congratulations on becoming an activated ColorStack member! 🎉 ' +
-      'From the team at ColorStack, we hope you enjoy your new merch! 🔥',
-    note: 'This was awarded for member activation.',
-    recipient: {
-      email: member.email,
-      firstName: member.firstName,
-      lastName: member.lastName,
-    },
-  });
-
-  if (!giftCardResult.ok) {
-    throw new Error(giftCardResult.error);
-  }
 
   job('gamification.activity.completed', {
     studentId: member.id,
