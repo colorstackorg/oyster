9b881d31a9129a8607afddf7859d784bbef7a6dc
Rami Abdou <ramiabdou98@gmail.com>
2025-07-01 16:18:56 -0700

    chore: add @remix-run/fs-routes and @remix-run/route-config dependencies in admin-dashboard and member-profile, and implement route configuration in new routes files

--- apps/member-profile/vite.config.ts
+++ apps/member-profile/vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig((env) => {
           v3_fetcherPersist: true,
           v3_lazyRouteDiscovery: true,
           v3_relativeSplatPath: true,
+          v3_routeConfig: true,
           v3_throwAbortReason: true,
         },
       }),
