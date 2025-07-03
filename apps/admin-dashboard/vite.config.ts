b6e775a049c89b569332920278805a45cf525054
Rami Abdou <38056800+ramiAbdou@users.noreply.github.com>
2025-07-03 14:53:34 -0700

    chore: upload sentry sourcemaps for vite applications 🗺️ (#859)

--- apps/admin-dashboard/vite.config.ts
+++ apps/admin-dashboard/vite.config.ts
@@ -1,8 +1,24 @@
+import { sentryVitePlugin } from '@sentry/vite-plugin';
 import { reactRouter } from '@react-router/dev/vite';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [reactRouter(), tsconfigPaths()],
+  plugins: [
+    reactRouter(),
+    tsconfigPaths(),
+    sentryVitePlugin({
+      org: 'colorstack',
+      project: 'admin-dashboard',
+      sourcemaps: { filesToDeleteAfterUpload: ['**/*.map'] },
+
+      ...(process.env.RAILWAY_GIT_COMMIT_SHA && {
+        release: {
+          name: `admin-dashboard@${process.env.RAILWAY_GIT_COMMIT_SHA}`,
+        },
+      }),
+    }),
+  ],
+  build: { sourcemap: true },
   server: { port: 3001 },
 });
--- apps/member-profile/vite.config.ts
+++ apps/member-profile/vite.config.ts
@@ -1,8 +1,24 @@
+import { sentryVitePlugin } from '@sentry/vite-plugin';
 import { reactRouter } from '@react-router/dev/vite';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [reactRouter(), tsconfigPaths()],
+  plugins: [
+    reactRouter(),
+    tsconfigPaths(),
+    sentryVitePlugin({
+      org: 'colorstack',
+      project: 'member-profile',
+      sourcemaps: { filesToDeleteAfterUpload: ['**/*.map'] },
+
+      ...(process.env.RAILWAY_GIT_COMMIT_SHA && {
+        release: {
+          name: `member-profile@${process.env.RAILWAY_GIT_COMMIT_SHA}`,
+        },
+      }),
+    }),
+  ],
+  build: { sourcemap: true },
   server: { port: 3000 },
 });
